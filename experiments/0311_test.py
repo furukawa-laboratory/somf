0292a477cc6090535e2cffab7b244086afd0e2df
IwasakiTohru <snowyandroid@gmail.com>
2017-07-04 15:41:47 +0900

    KSEクラス作成

---
+++ experiments/0311_test.py
@@ -0,0 +1,7 @@
+from lib.KSE0331 import KSE
+
+def _main():
+    kse = KSE()
+
+if __name__ == "__main__":
+    _main()
---
+++ lib/KSE0331.py
@@ -0,0 +1,4 @@
+
+class KSE(object):
+    def __init__(self):
+        print("hello")
