de353aabb357328dd531802ade74aba5d8660c7d
IwasakiTohru <snowyandroid@gmail.com>
2017-07-04 16:59:37 +0900

    鞍データのプロットまで

--- experiments/0331_fitting.py
+++ experiments/0331_fitting.py
@@ -1,7 +1,16 @@
-from lib.KSE0331 import KSE
+from lib.datasets.artificial import kura
+import matplotlib.pyplot as plt
+from lib.models.KSE import KSE
+
 
 def _main():
-    kse = KSE()
+    version = "0331"
+    X = kura.create_data(100)
+    plt.plot(X[:, 0], X[:, 1], '.')
+    plt.show()
+
+    kse = KSE(version, X)
+    kse.fit()
 
 if __name__ == "__main__":
     _main()
