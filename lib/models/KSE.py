aa0e3fabac90f38b4b8f4a354761713d3d739607
IwasakiTohru <snowyandroid@gmail.com>
2017-07-04 17:00:28 +0900

    Versionを引数にしてクラスを持ってくる

---
+++ lib/models/KSE.py
@@ -0,0 +1,8 @@
+from lib.models.KSE0331 import KSE as KSE0331
+
+
+def KSE(version, X):
+    if version == "0331":
+        return KSE0331(X)
+    else:
+        raise ValueError("Undefined Version {}".format(version))
