566d0d297e2fc9ec142d0a2d0f33a7dc22839359
IwasakiTohru <snowyandroid@gmail.com>
2017-07-04 19:22:12 +0900

    初期化まで作成

--- lib/models/iwasaki/KSE0331.py
+++ lib/models/iwasaki/KSE0331.py
@@ -2,6 +2,16 @@ import numpy as np
 
 
 class KSE(object):
-    def __init__(self):
-        self.x = 2
-        self.z = np.linspace(-1, 1)
+    def __init__(self, X, latent_dim, init='random'):
+        self.X = X.copy()
+        self.nb_samples = X.shape[0]
+        self.input_dim = X.shape[1]
+        self.latent_dim = latent_dim
+
+        self.Z = init
+
+        self.history = {}
+
+    def fit(self, nb_epoch, epsilon=0.5):
+
+        return self.history
