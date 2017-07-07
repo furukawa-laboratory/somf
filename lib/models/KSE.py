5b0f43ca493c589532cbf591f4b8014b9de71db2
IwasakiTohru <snowyandroid@gmail.com>
2017-07-07 16:33:06 +0900

    引数の記述忘れ

--- lib/models/KSE.py
+++ lib/models/KSE.py
@@ -1,8 +1,8 @@
 from lib.models.flab.KSE0331 import KSE as KSE0331
 
 
-def KSE(version, X):
+def KSE(version, X, latent_dim, init):
     if version == "0331":
-        return KSE0331(X)
+        return KSE0331(X, latent_dim, init)
     else:
         raise ValueError("Undefined Version {}".format(version))
