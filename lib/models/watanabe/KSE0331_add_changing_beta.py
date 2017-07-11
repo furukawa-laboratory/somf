230632c0b87f3bd691a1154c5cae2ec6c6f9cb2e
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-07-11 19:36:44 +0900

    betaの推定方法をクラス作成時に選択可能に．7月11日のミーティングで出たMAP推定のbetaの推定方法を選択可能．

---
+++ lib/models/watanabe/KSE0331_add_changing_beta.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+class KSE(object):
+    def __init__(self, X, latent_dim, init='random', choice_beta='type1'):
+        self.X = X.copy()
+        self.N = X.shape[0]
+        self.D = X.shape[1]
+        self.L = latent_dim
+
+        self.Z = None
+        if isinstance(init, str) and init in 'random':
+            self.Z = np.random.normal(0, 0.1, (self.N, self.L))
+        elif isinstance(init, np.ndarray) and init.shape == (self.N, self.L):
+            self.Z = init.copy()
+        else:
+            raise ValueError("invalid init: {}".format(init))
+
+        self.choice_beta = choice_beta
+        if choice_beta not in ['type1','type2']:
+            raise ValueError("invalid choice_beta: {}".format(choice_beta))
+
+        self.history = {}
+
+    def fit(self, nb_epoch=100, epsilon=0.5, gamma=1.0, sigma=30.0):
+
+        K = self.X @ self.X.T
+        X2 = np.diag(K)[:, None]
+        alpha = 1.0 / (sigma ** 2)
+
+        self.history['z'] = np.zeros((nb_epoch, self.N, self.L))
+        self.history['y'] = np.zeros((nb_epoch, self.N, self.D))
+
+        for epoch in range(nb_epoch):
+            Delta = self.Z[:, None, :] - self.Z[None, :, :]
+            Dist = np.sum(np.square(Delta), axis=2)
+            H = np.exp(-0.5 * gamma * Dist)
+            H -= H * np.identity(self.N)
+            Hprime = H
+            G = np.sum(H, axis=1)[:, None]
+            GInv = 1 / G
+            R = H * GInv
+            Rprime = Hprime * GInv
+
+            Y = R @ self.X
+            if self.choice_beta is 'type1':
+                Y2 = np.sum(np.square(Y), axis=1)[:, None]
+                beta0 = np.sum(G) / np.sum(G * (X2 - Y2))
+            elif self.choice_beta is 'type2':
+                beta0 = (self.N * self.D) / np.sum((X - Y)**2.0)
+            beta = (G / (1 + G)) * beta0
+
+            U = R - np.identity(self.N)
+            Phi = U @ K
+            PhiBar = np.sum(R * Phi, axis=1)[:, None]
+            E = np.diag(U @ K @ U.T)[:, None]
+
+            if self.choice_beta is 'type1':
+                Y2 = np.sum(np.square(Y), axis=1)[:, None]
+                beta0 = np.sum(G) / np.sum(G * (X2 - Y2))
+            elif self.choice_beta is 'type2':
+                beta0 = (self.N * self.D) / np.sum(E)
+            beta = (G / (1 + G)) * beta0
+
+            A = Rprime * (beta * (Phi - PhiBar))
+            A += Rprime * (0.5 * (beta * E - 1.0) / (1.0 + G))
+
+            dZ = np.sum((A + A.T)[:, :, None] * Delta, axis=1)
+            dZ -= (alpha / gamma) * self.Z
+
+            self.Z += epsilon * dZ
+
+            self.history['z'][epoch] = self.Z
+            self.history['y'][epoch] = Y
+
+        return self.history
