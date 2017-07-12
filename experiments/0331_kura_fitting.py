c295579fa195ab2871eb944981db4e53777abd54
IwasakiTohru <snowyandroid@gmail.com>
2017-07-12 13:49:15 +0900

    実験のコードでグラフ表示の細部を記述しない

--- experiments/0331_kura_fitting.py
+++ experiments/0331_kura_fitting.py
@@ -1,7 +1,6 @@
 from lib.datasets.artificial import kura
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from lib.models.KSE import KSE
+from lib.graphics.KSEViewer import KSEViewer
 
 
 def _main():
@@ -12,14 +11,9 @@ def _main():
 
     kse = KSE(version, X, latent_dim=latent_dim, init=init)
     kse.fit()
-    Y = kse.history['y'][-1]
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:, 0], X[:, 1], X[:, 2], label='X')
-    ax.scatter(Y[:, 0], Y[:, 1], Y[:, 2], label='Y', c='r')
-    plt.legend()
-    plt.show()
+    viewer = KSEViewer(kse)
+    viewer.draw()
 
 
 if __name__ == "__main__":
