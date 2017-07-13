0824f22699970514db56f39fae77a4cc260fe998
IwasakiTohru <snowyandroid@gmail.com>
2017-07-13 14:47:50 +0900

    観測空間のデフォルトスタイル変更

--- lib/graphics/observation_space.py
+++ lib/graphics/observation_space.py
@@ -2,6 +2,8 @@ class ObservationSpace(object):
     def __init__(self, axes, kse):
         self.axes = axes
         self.kse = kse
+        self.x_kws = 'k+'
+        self.y_kws = 'r.'
 
     def update(self, epoch):
         Y = self.kse.history['y'][epoch, :, :]
@@ -9,7 +11,7 @@ class ObservationSpace(object):
         self.graph_y.set_ydata(Y[:, 1])
 
     def init(self):
-        self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], 'o', label="$X$")
+        self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.x_kws, label="$X$")
         Y = self.kse.history['y'][0, :, :]
-        self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], 'x', label="Y")
+        self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], self.y_kws, label="Y")
         self.axes.legend(loc='upper right')
