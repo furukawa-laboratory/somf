70955a263c52fd0c0768ce29405273c62aadc613
IwasakiTohru <snowyandroid@gmail.com>
2017-07-13 14:48:52 +0900

    変数名修正

--- lib/graphics/observation_space.py
+++ lib/graphics/observation_space.py
@@ -2,8 +2,8 @@ class ObservationSpace(object):
     def __init__(self, axes, kse):
         self.axes = axes
         self.kse = kse
-        self.x_kws = 'k+'
-        self.y_kws = 'r.'
+        self.x_style = 'k+'
+        self.y_style = 'r.'
 
     def update(self, epoch):
         Y = self.kse.history['y'][epoch, :, :]
@@ -11,7 +11,7 @@ class ObservationSpace(object):
         self.graph_y.set_ydata(Y[:, 1])
 
     def init(self):
-        self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.x_kws, label="$X$")
+        self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.x_style, label="$X$")
         Y = self.kse.history['y'][0, :, :]
-        self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], self.y_kws, label="Y")
+        self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], self.y_style, label="Y")
         self.axes.legend(loc='upper right')
