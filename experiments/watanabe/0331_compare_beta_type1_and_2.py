4db743c95d7fd57cd385927468cc8344551558b3
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-07-13 18:26:33 +0900

    beta比較実行ファイルの軽微な変更

--- experiments/watanabe/0331_compare_beta_type1_and_2.py
+++ experiments/watanabe/0331_compare_beta_type1_and_2.py
@@ -15,19 +15,20 @@ def _main():
 
     X += np.random.normal(0, 0.1, X.shape)
 
-    nb_epoch = 100
+    nb_epoch = 500
 
     kse1 = KSE(X, latent_dim=latent_dim, init=init, choice_beta=betaType1)
     kse2 = KSE(X, latent_dim=latent_dim, init=init, choice_beta=betaType2)
     kse1.fit(nb_epoch = nb_epoch)
     kse2.fit(nb_epoch = nb_epoch)
 
-    viewer = KSEViewer(kse1, rows=2, cols=2, figsize=(10, 6))
+    viewer = KSEViewer(kse1, rows=2, cols=2, figsize=(10, 6),skip=10)
     viewer.add_observation_space(kse=kse1, row=1, col=1, aspect='equal', title=betaType1)
     viewer.add_sequential_space(kse=kse1, subject_name_list=['gamma', 'beta'], row=2, col=1)
     viewer.add_observation_space(kse=kse2, row=1, col=2, aspect='equal', title=betaType2)
     viewer.add_sequential_space(kse=kse2, subject_name_list=['gamma', 'beta'], row=2, col=2)
     viewer.draw()
+    #viewer.save_gif('compare_beta_type.gif')
 
 if __name__ == "__main__":
     _main()
