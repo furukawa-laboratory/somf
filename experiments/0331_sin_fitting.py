0f040a29ad151a9e273f23bd0adaeeb7971d01e1
IwasakiTohru <snowyandroid@gmail.com>
2017-07-13 14:23:34 +0900

    sinデータフィッティングの実験を追加

---
+++ experiments/0331_sin_fitting.py
@@ -0,0 +1,26 @@
+from lib.datasets.artificial import sin
+from lib.models.KSE import KSE
+from lib.graphics.KSEViewer import KSEViewer
+import numpy as np
+
+
+def _main():
+    version = "0331"
+    X = sin.create_data(100)
+    latent_dim = 2
+    init = 'random'
+
+    np.random.seed(100)
+    X += np.random.normal(0, 0.1, X.shape)
+
+    kse = KSE(version, X, latent_dim=latent_dim, init=init)
+    kse.fit()
+
+    viewer = KSEViewer(kse, rows=2, cols=1)
+    viewer.add_observation_space(row=1, col=1, aspect='equal')
+    viewer.add_sequential_space(row=2, col=1)
+    viewer.draw()
+
+
+if __name__ == "__main__":
+    _main()
