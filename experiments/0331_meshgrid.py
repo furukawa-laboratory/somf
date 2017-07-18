b13b9925c312f6c89381659c976f245642c90220
IwasakiTohru <snowyandroid@gmail.com>
2017-07-18 11:50:12 +0900

    メッシュ表示確認用のコード修正
    以下のメッシュ表示を確認
    左上: 3次元空間中の2次元メッシュ
    右上: 3次元空間中の1次元メッシュ
    左下: 2次元空間中の2次元メッシュ
    右下: 2次元空間中の1次元メッシュ

--- experiments/0331_meshgrid.py
+++ experiments/0331_meshgrid.py
@@ -7,7 +7,7 @@ import numpy as np
 def _main():
     np.random.seed(100)
     kura_data = kura.create_data(100)
-    sin_data = sin.create_data(100)
+    sin_data = sin.create_data(100, input_dim=3)
 
     kse_kura = KSE(kura_data, latent_dim=2, init='random')
     kse_sin = KSE(sin_data, latent_dim=1, init='random')
@@ -15,11 +15,11 @@ def _main():
     kse_kura.fit()
     kse_sin.fit()
 
-    viewer = KSEViewer(kse_kura, rows=2, cols=2, figsize=(10, 6))
+    viewer = KSEViewer(kse_kura, rows=2, cols=2, figsize=(6, 6))
     viewer.add_observation_space(kse=kse_kura, row=1, col=1, aspect='equal', projection='3d')
-    viewer.add_sequential_space(kse=kse_kura, subject_name_list=['gamma', 'beta'], row=2, col=1)
-    viewer.add_observation_space(kse=kse_sin, row=1, col=2, aspect='equal')
-    viewer.add_sequential_space(kse=kse_sin, subject_name_list=['gamma', 'beta'], row=2, col=2)
+    viewer.add_observation_space(kse=kse_kura, row=2, col=1, aspect='equal')
+    viewer.add_observation_space(kse=kse_sin, row=1, col=2, aspect='equal', projection='3d')
+    viewer.add_observation_space(kse=kse_sin, row=2, col=2, aspect='equal')
     viewer.draw()
 
 
