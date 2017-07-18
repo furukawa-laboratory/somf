f804f4e9517fc12dd36ade1ac16f2595867646f4
IwasakiTohru <snowyandroid@gmail.com>
2017-07-18 11:45:12 +0900

    不要になったメソッドを削除

--- lib/graphics/observation_space.py
+++ lib/graphics/observation_space.py
@@ -26,23 +26,6 @@ class ObservationSpace(object):
         else:
             raise ValueError('invalid dim view:{0}, latent{1}'.format(self.view_dim, self.latent_dim))
 
-    def update(self, epoch):
-        Y = self.kse.history['y'][epoch, :, :]
-        self.graph_y.set_xdata(Y[:, 0])
-        self.graph_y.set_ydata(Y[:, 1])
-        if self.axes.name == '3d':
-            self.graph_y.set_3d_properties(Y[:, 2])
-
-        if self.show_f:
-            F = self.kse.history['f'][epoch, :, :]
-            if self.axes.name == '3d':
-                F = F.reshape(self.kse.resolution, self.kse.resolution, -1)
-                self.axes.collections.remove(self.graph_f)
-                self.graph_f = self.axes.plot_wireframe(F[:, :, 0], F[:, :, 1], F[:, :, 2], color=self.f_style[0], label="$F$")
-            else:
-                self.graph_f.set_xdata(F[:, 0])
-                self.graph_f.set_ydata(F[:, 1])
-
     def update_3dview_2dmesh(self, epoch):
         Y = self.kse.history['y'][epoch, :, :]
         self.graph_y.set_xdata(Y[:, 0])
@@ -62,25 +45,6 @@ class ObservationSpace(object):
             F = self.kse.history['f'][epoch, :, :]
             self.graph_f.set_xdata(F[:, 0])
             self.graph_f.set_ydata(F[:, 1])
-
-    def init(self):
-        Y = self.kse.history['y'][0, :, :]
-        if self.show_f:
-            F = self.kse.history['f'][0, :, :]
-
-        if self.axes.name == '3d':
-            self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.kse.X[:, 2], self.x_style, label="$X$")
-            if self.show_f:
-                F = F.reshape(self.kse.resolution, self.kse.resolution, -1)
-                self.graph_f = self.axes.plot_wireframe(F[:, :, 0], F[:, :, 1], F[:, :, 2], color=self.f_style[0], label="$F$")
-            self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], Y[:, 2], self.y_style, label="Y")
-        else:
-            self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.x_style, label="$X$")
-            if self.show_f:
-                self.graph_f, = self.axes.plot(F[:, 0], F[:, 1], self.f_style, label="$F$")
-            self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], self.y_style, label="Y")
-
-        self.axes.legend(loc='upper right')
 
     def init_3dview_2dmesh(self):
         self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.kse.X[:, 2], self.x_style, label="$X$")
