d1716385d69408dc645d4022532dde1306be6697
IwasakiTohru <snowyandroid@gmail.com>
2017-07-18 11:38:31 +0900

    表示状況ごとのメソッドを追加

--- lib/graphics/observation_space.py
+++ lib/graphics/observation_space.py
@@ -27,6 +27,26 @@ class ObservationSpace(object):
                 self.graph_f.set_xdata(F[:, 0])
                 self.graph_f.set_ydata(F[:, 1])
 
+    def update_3dview_2dmesh(self, epoch):
+        Y = self.kse.history['y'][epoch, :, :]
+        self.graph_y.set_xdata(Y[:, 0])
+        self.graph_y.set_ydata(Y[:, 1])
+        self.graph_y.set_3d_properties(Y[:, 2])
+        if self.show_f:
+            F = self.kse.history['f'][epoch, :, :]
+            F = F.reshape(self.kse.resolution, self.kse.resolution, -1)
+            self.axes.collections.remove(self.graph_f)
+            self.graph_f = self.axes.plot_wireframe(F[:, :, 0], F[:, :, 1], F[:, :, 2], color=self.f_style[0], label="$F$")
+
+    def update_2dview_1dmesh(self, epoch):
+        Y = self.kse.history['y'][epoch, :, :]
+        self.graph_y.set_xdata(Y[:, 0])
+        self.graph_y.set_ydata(Y[:, 1])
+        if self.show_f:
+            F = self.kse.history['f'][epoch, :, :]
+            self.graph_f.set_xdata(F[:, 0])
+            self.graph_f.set_ydata(F[:, 1])
+
     def init(self):
         Y = self.kse.history['y'][0, :, :]
         if self.show_f:
@@ -44,4 +64,23 @@ class ObservationSpace(object):
                 self.graph_f, = self.axes.plot(F[:, 0], F[:, 1], self.f_style, label="$F$")
             self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], self.y_style, label="Y")
 
+        self.axes.legend(loc='upper right')
+
+    def init_3dview_2dmesh(self):
+        self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.kse.X[:, 2], self.x_style, label="$X$")
+        if self.show_f:
+            F = self.kse.history['f'][0, :, :]
+            F = F.reshape(self.kse.resolution, self.kse.resolution, -1)
+            self.graph_f = self.axes.plot_wireframe(F[:, :, 0], F[:, :, 1], F[:, :, 2], color=self.f_style[0], label="$F$")
+        Y = self.kse.history['y'][0, :, :]
+        self.graph_y, = self.axes.plot(Y[:, 0], Y[:, 1], Y[:, 2], self.y_style, label="Y")
+        self.axes.legend(loc='upper right')
+
+    def init_2dview_1dmesh(self):
+        self.axes.plot(self.kse.X[:, 0], self.kse.X[:, 1], self.x_style, label="$X$")
+        if self.show_f:
+            F = self.kse.history['f'][0, :, :]
+            self.graph_f = self.axes.plot(F[:, 0], F[:, 1], self.f_style, label="$F$")[0]
+        Y = self.kse.history['y'][0, :, :]
+        self.graph_y = self.axes.plot(Y[:, 0], Y[:, 1], self.y_style, label="Y")[0]
         self.axes.legend(loc='upper right')
