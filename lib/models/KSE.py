19da90f94aa125ca59645767b6f616f7f6111f9e
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-07-23 19:54:52 +0900

    331と標準版を比較

--- lib/models/KSE.py
+++ lib/models/KSE.py
@@ -1,8 +1,11 @@
 from lib.models.flab.KSE0331 import KSE as KSE0331
+from lib.models.flab.KSEstandard import KSE as KSEstandard
 
 
 def KSE(version, X, latent_dim, init):
     if version == "0331":
         return KSE0331(X, latent_dim, init)
+    elif version == "standard":
+        return KSEstandard(X, latent_dim, init)
     else:
         raise ValueError("Undefined Version {}".format(version))
--- tests/0331_vs_standard_kura_fitting.py
+++ tests/0331_vs_standard_kura_fitting.py
@@ -5,18 +5,23 @@ import numpy as np
 
 
 def _main():
-    version = "0331"
     np.random.seed(100)
     X = kura.create_data(100)
     latent_dim = 2
-    init = 'random'
+    init = np.random.normal(0, 0.1, (X.shape[0], latent_dim))
 
-    kse = KSE(version, X, latent_dim=latent_dim, init=init)
-    kse.fit()
+    kse0331 = KSE("0331", X, latent_dim=latent_dim, init=init)
+    kse_standard = KSE("standard", X, latent_dim=latent_dim, init=init)
 
-    viewer = KSEViewer(kse, rows=2, cols=1, figsize=(6, 6))
-    viewer.add_observation_space(row=1, col=1, aspect='equal', projection='3d')
-    viewer.add_sequential_space(['gamma', 'beta'], row=2, col=1)
+    nb_epoch = 500
+    kse0331.fit(nb_epoch = nb_epoch)
+    kse_standard.fit(nb_epoch = nb_epoch)
+
+    viewer = KSEViewer(kse0331, rows=2, cols=2, figsize=(10, 6),skip=10)
+    viewer.add_observation_space(kse=kse0331, row=1, col=1, aspect='equal', projection='3d')
+    viewer.add_sequential_space(kse=kse0331,subject_name_list=['gamma', 'beta'], row=2, col=1)
+    viewer.add_observation_space(kse=kse_standard,row=1, col=2, aspect='equal', projection='3d')
+    viewer.add_sequential_space(kse=kse_standard,subject_name_list=['gamma', 'beta'], row=2, col=2)
     viewer.draw()
 
 
