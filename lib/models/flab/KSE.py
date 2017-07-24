424d6af2f4bf4d57eae11101ef77dcc2b0b1d691
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-07-24 21:10:17 +0900

    428を追加

--- lib/models/flab/KSE.py
+++ lib/models/flab/KSE.py
@@ -1,5 +1,6 @@
 from lib.models.flab.KSE0331 import KSE as KSE0331
 from lib.models.flab.KSEstandard import KSE as KSEstandard
+from lib.models.flab.KSE0428 import KSE as KSE0428
 
 
 def KSE(version, X, latent_dim, init):
@@ -7,5 +8,7 @@ def KSE(version, X, latent_dim, init):
         return KSE0331(X, latent_dim, init)
     elif version == "standard":
         return KSEstandard(X, latent_dim, init)
+    if version == "0428":
+        return KSE0428(X, latent_dim, init)
     else:
         raise ValueError("Undefined Version {}".format(version))
