d178df84d3f3b71a637892688feb565e1cc9afd1
IwasakiTohru <snowyandroid@gmail.com>
2017-07-26 10:13:17 +0900

    検証したい観測空間の次元を全て検証する

--- experiments/0725Mtg/D100/0524_sin.py
+++ experiments/0725Mtg/D100/0524_sin.py
@@ -6,23 +6,25 @@ from lib.models.flab.KSE import KSE
 
 
 def _main():
-    input_dim = 2
-    np.random.seed(100)
-    X = sin.create_data(100, input_dim=input_dim)
-    X += np.random.normal(0.0, 0.3, X.shape) / input_dim
-    latent_dim = 1
-    init = np.random.normal(0, 0.01, (X.shape[0], latent_dim))
+    input_dims = [2, 3, 5, 10, 100]
+    for input_dim in input_dims:
+        print(input_dim)
+        np.random.seed(100)
+        X = sin.create_data(100, input_dim=input_dim)
+        X += np.random.normal(0.0, 0.3, X.shape) / input_dim
+        latent_dim = 1
+        init = np.random.normal(0, 0.01, (X.shape[0], latent_dim))
 
-    kse0524 = KSE("0524", X, latent_dim=latent_dim, init=init)
+        kse0524 = KSE("0524", X, latent_dim=latent_dim, init=init)
 
-    nb_epoch = 5000
-    kse0524.fit(nb_epoch = nb_epoch, gamma_divisor=input_dim, gamma_update_freq=5)
+        nb_epoch = 5000
+        kse0524.fit(nb_epoch = nb_epoch, gamma_divisor=input_dim, gamma_update_freq=5)
 
-    viewer = KSEViewer(kse0524, rows=2, cols=1, figsize=(5, 5),skip=10)
-    viewer.add_observation_space(kse=kse0524, row=1, col=1, aspect='equal')
-    viewer.add_sequential_space(kse=kse0524, subject_name_list=['gamma', 'beta'], row=2, col=1)
-    viewer.draw()
-
+        viewer = KSEViewer(kse0524, rows=2, cols=1, figsize=(10, 5),skip=10)
+        viewer.add_observation_space(kse=kse0524, row=1, col=1, aspect='equal')
+        viewer.add_sequential_space(kse=kse0524, subject_name_list=['gamma', 'beta'], row=2, col=1)
+        viewer.draw()
+        viewer.save_png(filename='524_D_{}.png'.format(input_dim))
 
 if __name__ == "__main__":
     _main()
