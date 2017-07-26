a0a5ceedfde7d57383de6928009e2aa7f9441f14
IwasakiTohru <snowyandroid@gmail.com>
2017-07-26 10:15:37 +0900

    検証用にgammaをDで割るアルゴリズム・2で割るアルゴリズムの2種類を用意する

--- experiments/0725Mtg/D100/0524_sin.py
+++ experiments/0725Mtg/D100/0524_sin.py
@@ -15,14 +15,18 @@ def _main():
         latent_dim = 1
         init = np.random.normal(0, 0.01, (X.shape[0], latent_dim))
 
-        kse0524 = KSE("0524", X, latent_dim=latent_dim, init=init)
+        kse0524_divD = KSE("0524", X, latent_dim=latent_dim, init=init)
+        kse0524_div2 = KSE("0524", X, latent_dim=latent_dim, init=init)
 
         nb_epoch = 5000
-        kse0524.fit(nb_epoch = nb_epoch, gamma_divisor=input_dim, gamma_update_freq=5)
+        kse0524_divD.fit(nb_epoch = nb_epoch, gamma_divisor=input_dim, gamma_update_freq=5)
+        kse0524_div2.fit(nb_epoch = nb_epoch, gamma_divisor=2, gamma_update_freq=5)
 
-        viewer = KSEViewer(kse0524, rows=2, cols=1, figsize=(10, 5),skip=10)
-        viewer.add_observation_space(kse=kse0524, row=1, col=1, aspect='equal')
-        viewer.add_sequential_space(kse=kse0524, subject_name_list=['gamma', 'beta'], row=2, col=1)
+        viewer = KSEViewer(kse0524_divD, rows=2, cols=2, figsize=(10, 5),skip=100)
+        viewer.add_observation_space(kse=kse0524_divD, row=1, col=1, aspect='equal', title='$\gamma = a/D$ $(D={})$'.format(input_dim))
+        viewer.add_sequential_space(kse=kse0524_divD, subject_name_list=['gamma', 'beta'], row=2, col=1)
+        viewer.add_observation_space(kse=kse0524_div2, row=1, col=2, aspect='equal', title='$\gamma = a/2$')
+        viewer.add_sequential_space(kse=kse0524_div2, subject_name_list=['gamma', 'beta'], row=2, col=2)
         viewer.draw()
         viewer.save_png(filename='524_D_{}.png'.format(input_dim))
 
