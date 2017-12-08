0c93e60780e9ecf39dc6061184524754b69f617f
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-12-08 18:00:01 +0900

    SOMクラス修正に伴うチュートリアルコードの修正

--- tutorials/som/animal_umatrix.py
+++ tutorials/som/animal_umatrix.py
@@ -10,7 +10,7 @@ from libs.datasets.artificial import animal
 
 
 if __name__ == '__main__':
-    T = 300
+    nb_epoch = 300
     resolution = 10
     sigma_max = 2.2
     sigma_min = 0.4
@@ -26,8 +26,7 @@ if __name__ == '__main__':
     np.random.seed(seed)
 
     som = SOM(X, latent_dim=latent_dim, resolution=resolution, sigma_max=sigma_max, sigma_min=sigma_min, tau=tau)
-    for t in tqdm(range(T)):
-        som.learning(t)
+    som.fit(nb_epoch=nb_epoch)
 
     som_umatrix = SOM_Umatrix(z=som.Z, x=X, resolution=umat_resolution, sigma=sigma_min, labels=labels)
     som_umatrix.draw_umatrix()
