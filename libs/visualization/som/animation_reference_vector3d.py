a2ea5afd853573f16207bd3a8df9afd4f4f4a38a
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-12-08 17:55:19 +0900

    SOMクラスの修正に伴いD=3, L=2のときの観測空間の描画ライブラリを修正

--- libs/visualization/som/animation_reference_vector3d.py
+++ libs/visualization/som/animation_reference_vector3d.py
@@ -5,7 +5,7 @@ import matplotlib.animation
 
 def update_graph(t,X,re_allY,ax,wframe,title):
     ax.cla()
-    re_Y=re_allY[:,:,:,t]
+    re_Y=re_allY[t, :, :, :]
     """
     graph.set_data(data[:,:,0], data[:,:,1])
     graph.set_3d_properties(data[:,:,2])
@@ -14,17 +14,19 @@ def update_graph(t,X,re_allY,ax,wframe,title):
     wframe = ax.plot_wireframe(re_Y[:,:,0],re_Y[:,:,1],re_Y[:,:,2],color='b')
     title.set_text('SOM, time={}'.format(t))
 
-def anime_reference_vector_3d(X,allY,resolution):
+def anime_reference_vector_3d(X,allY):
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     title = ax.set_title('3D Test')
 
-    D = allY.shape[1]
-    T = allY.shape[2]
-    re_allY=allY.reshape((resolution,resolution,D,T))
+    K = allY.shape[1]
+    D = allY.shape[2]
+    T = allY.shape[0]
+    resolution = np.int(np.sqrt(K))
+    re_allY=allY.reshape((T, resolution, resolution, D))
     ax.scatter(X[:,0],X[:,1],X[:,2], color='g')
-    wframe = ax.plot_wireframe(re_allY[:,:,0,0],re_allY[:,:,1,0],re_allY[:,:,2,0],color='b')
+    wframe = ax.plot_wireframe(re_allY[0,:,:,0],re_allY[0,:,:,1],re_allY[0,:,:,2],color='b')
     ax.set_xlim(X[:,0].min(),X[:,0].max())
     ax.set_ylim(X[:,1].min(),X[:,1].max())
     ax.set_zlim(X[:,2].min(),X[:,2].max())
