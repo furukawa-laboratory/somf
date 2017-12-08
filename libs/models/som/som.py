d7d18dfcf00ea89c6dcdd4fe48fafe50d39f7f73
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-12-08 17:04:46 +0900

    SOMクラスをsklearn風に修正
    fitを実行したら学習を実行する．外部でfor文を回す必要はない

--- libs/models/som/som.py
+++ libs/models/som/som.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from scipy.spatial import distance as dist
+from tqdm import tqdm
 
 class SOM:
     def __init__(self, X, latent_dim, resolution, sigma_max, sigma_min, tau, init='random'):
@@ -11,41 +12,63 @@ class SOM:
         self.sigma_min = sigma_min
         self.tau = tau
 
-        if isinstance(init, str) and init == 'random':
-            self.Z = np.random.rand(self.N, latent_dim) * 2.0 - 1.0
-        elif isinstance(init, np.ndarray) and init.shape == (self.N, latent_dim):
-            self.Z = init.copy()
-        else:
-            raise ValueError("invalid init: {}".format(init))
 
         if latent_dim == 1:
+            self.L = latent_dim
             self.Zeta = np.linspace(-1.0, 1.0, resolution)[:,np.newaxis]
         elif latent_dim == 2:
+            self.L = latent_dim
             zeta = np.meshgrid(np.linspace(-1, 1, resolution), np.linspace(-1, 1, resolution))
             self.Zeta = np.dstack(zeta).reshape(resolution**2, latent_dim)
         else:
             raise ValueError("invalid latent dimension: {}".format(latent_dim))
 
-    def learning(self, t):
-        # 協調過程
-        # 学習量を計算
-        sigma = self.sigma_min + (self.sigma_max - self.sigma_min) * np.exp(-t / self.tau) # 近傍半径を設定
-        Dist = dist.cdist(self.Zeta, self.Z, 'sqeuclidean')
-        # KxNの距離行列を計算
-        # ノードと勝者ノードの全ての組み合わせにおける距離を網羅した行列
-        H = np.exp(-Dist / (2 * sigma * sigma)) # KxNの学習量行列を計算
+        if isinstance(init, str) and init == 'random':
+            self.Z = np.random.rand(self.N, latent_dim) * 2.0 - 1.0
+        elif isinstance(init, str) and init == 'random_bmu':
+            init_bmus = np.random.randint(0, Zeta.shape[0] - 1, self.N)
+            self.Z = self.Zeta[init_bmus,:]
+        elif isinstance(init, np.ndarray) and init.shape == (self.N, latent_dim):
+            self.Z = init.copy()
+        else:
+            raise ValueError("invalid init: {}".format(init))
 
-        # 適合過程
-        # 参照ベクトルの更新
-        G = np.sum(H, axis=1)[:, np.newaxis] # 各ノードが受ける学習量の総和を保持するKx1の列ベクトルを計算
-        Ginv = np.reciprocal(G) # Gのそれぞれの要素の逆数を取る
-        R = H * Ginv # 学習量の総和が1になるように規格化
-        self.Y = R @ self.X # 学習量を重みとして観測データの平均を取り参照ベクトルとする
+        self.history = {}
 
-        # 競合過程
-        # 勝者ノードの計算
-        Dist = dist.cdist(self.X, self.Y) #NxKの距離行列を計算
-        bmus = Dist.argmin(axis=1)
-        # Nx1の勝者ノード番号をまとめた列ベクトルを計算
-        # argmin(axis=1)を用いて各行で最小値を探しそのインデックスを返す
-        self.Z = self.Zeta[bmus, :] # 勝者ノード番号から勝者ノードを求める
+    def fit(self, nb_epoch=100, verbose=True):
+
+        self.history['z'] = np.zeros((nb_epoch, self.N, self.L))
+        self.history['y'] = np.zeros((nb_epoch, self.N, self.D))
+
+        if verbose:
+            bar = tqdm(range(nb_epoch))
+        else:
+            bar = range(nb_epoch)
+
+        for epoch in bar:
+            # 協調過程
+            # 学習量を計算
+            #sigma = self.sigma_min + (self.sigma_max - self.sigma_min) * np.exp(-epoch / self.tau) # 近傍半径を設定
+            sigma = self.sigma_min + (self.sigma_max - self.sigma_min) * ( 1 - (epoch / self.tau) ) # 近傍半径を設定
+            Dist = dist.cdist(self.Zeta, self.Z, 'sqeuclidean')
+            # KxNの距離行列を計算
+            # ノードと勝者ノードの全ての組み合わせにおける距離を網羅した行列
+            H = np.exp(-Dist / (2 * sigma * sigma)) # KxNの学習量行列を計算
+
+            # 適合過程
+            # 参照ベクトルの更新
+            G = np.sum(H, axis=1)[:, np.newaxis] # 各ノードが受ける学習量の総和を保持するKx1の列ベクトルを計算
+            Ginv = np.reciprocal(G) # Gのそれぞれの要素の逆数を取る
+            R = H * Ginv # 学習量の総和が1になるように規格化
+            self.Y = R @ self.X # 学習量を重みとして観測データの平均を取り参照ベクトルとする
+
+            # 競合過程
+            # 勝者ノードの計算
+            Dist = dist.cdist(self.X, self.Y) #NxKの距離行列を計算
+            bmus = Dist.argmin(axis=1)
+            # Nx1の勝者ノード番号をまとめた列ベクトルを計算
+            # argmin(axis=1)を用いて各行で最小値を探しそのインデックスを返す
+            self.Z = self.Zeta[bmus, :] # 勝者ノード番号から勝者ノードを求める
+
+            self.history['z'][epoch] = self.Z
+            self.history['y'][epoch] = self.Y
