37b1fde0a68e10f3517b65ffc4fd6cf4f946685c
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-12-10 23:40:01 +0900

    動物データのSOMによる学習における学習過程をPCAを用いて描画するチュートリアルプログラムを作成

---
+++ tutorials/som/animal_visualize_learning_process_by_pca.py
@@ -0,0 +1,35 @@
+import numpy as np
+from tqdm import tqdm
+
+import sys
+sys.path.append('../../')
+
+from libs.models.som.som import SOM
+from libs.datasets.artificial import animal
+from libs.visualization.som.animation_learning_process_3d import anime_learning_process_3d
+
+
+if __name__ == '__main__':
+    nb_epoch = 100
+    resolution = 10
+    sigma_max = 2.2
+    sigma_min = 0.4
+    tau = 50
+    latent_dim = 2
+    seed = 1
+
+    title_text="animal_map_learning_process"
+    save_gif = False
+
+    X, labels = animal.load_data()
+
+    X -= X.mean(axis=0)
+
+    np.random.seed(seed)
+
+    som = SOM(X, latent_dim=latent_dim, resolution=resolution,
+              sigma_max=sigma_max, sigma_min=sigma_min, tau=tau)
+    som.fit(nb_epoch=nb_epoch)
+
+    anime_learning_process_3d(X=som.X, Y_allepoch=som.history['y'], labels=labels,
+                              title_text=title_text, save_gif=save_gif)
