bd2a0c260987705bd7b0408259dd80018f8ccc28
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2017-12-10 23:39:14 +0900

    ファイル名変更に伴う修正

--- tutorials/som/fitting_kura.py
+++ tutorials/som/fitting_kura.py
@@ -2,26 +2,28 @@ import numpy as np
 import sys
 sys.path.append('../../')
 from libs.models.som.som import SOM
-from libs.visualization.som.animation_reference_vector3d import anime_reference_vector_3d
+from libs.visualization.som.animation_learning_process_3d import anime_learning_process_3d
 from libs.datasets.artificial.kura import create_data
 if __name__ == '__main__':
-    nb_epoch = 300
+    nb_epoch = 150
     resolution = 20
     SIGMA_MAX = 2.2
     SIGMA_MIN = 0.1
     TAU = 50
     latent_dim = 2
 
-    nb_samples = 300
+    x_sigma = 0.1
+
+    nb_samples = 500
     seed = 1
 
     # X = np.loadtxt("../../data/kura.txt")  # reading observation data
     np.random.seed(seed)
     X = create_data(nb_samples)
-    D = X.shape[1]
+    X += np.random.normal(0,0.1,X.shape)
 
     som = SOM(X, latent_dim=latent_dim, resolution=resolution, sigma_max=SIGMA_MAX, sigma_min=SIGMA_MIN, tau=TAU)
 
     som.fit(nb_epoch=nb_epoch)
 
-    anime_reference_vector_3d(X=som.X, allY=som.history['y'])
+    anime_learning_process_3d(X=som.X, Y_allepoch=som.history['y'])
