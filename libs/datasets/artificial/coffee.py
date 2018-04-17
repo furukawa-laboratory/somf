6779e8c72919654b815515e2b8dbf26f1b262710
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2018-04-18 01:49:10 +0900

    コーヒーのデータをartificial dataに追加

---
+++ libs/datasets/artificial/coffee.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+
+
+def load_data(retlabel=True):
+    datastore_name = 'datastore/coffee'
+    file_name = 'features.txt'
+
+    directory_path = os.path.join(os.path.dirname(__file__), datastore_name)
+    file_path = os.path.join(directory_path, file_name)
+
+    x = np.loadtxt(file_path)
+
+    if retlabel:
+        label_name = 'labels.txt'
+        label_path = os.path.join(directory_path, label_name)
+        label = np.genfromtxt(label_path, dtype=str)
+
+        return x, label
+    else:
+        return x
