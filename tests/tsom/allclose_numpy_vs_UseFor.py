ddfef49f922a8de29c22715368025ff4b1ea2768
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2018-09-16 18:42:56 +0900

    for文実装とのペアプロ用のテスト関数を作成

--- tests/tsom/allclose_numpy_vs_UseFor.py
+++ tests/tsom/allclose_numpy_vs_UseFor.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from libs.models.tsom.tsom import TSOM2
-from libs.models.som.som_use_for import SOMUseFor
+from libs.models.tsom.tsom_use_for import TSOM2UseFor
 
 
 class TestTSOM2(unittest.TestCase):
@@ -17,9 +17,9 @@ class TestTSOM2(unittest.TestCase):
         nb_samples2 = 35
         observed_dim = 5
 
-        X = np.random.normal(0, 1, (nb_samples1,nb_samples2,observed_dim))
+        X = np.random.normal(0, 1, (nb_samples1, nb_samples2, observed_dim))
 
-
+        # set learning parameter
         nb_epoch = 60
         latent_dim = [1, 2]
         resolution = [20, 30]
@@ -27,16 +27,28 @@ class TestTSOM2(unittest.TestCase):
         sigma_min = [0.4, 0.2]
         tau = [50, 60]
 
-        # prepare init
-        Z1init = np.random.rand(nb_samples1,)
-        Z2init =
-        som_numpy = SOM(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit)
-        som_use_for = SOMUseFor(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit)
+        ## prepare init
+        Z1init = np.random.rand(nb_samples1, latent_dim[0])
+        Z2init = np.random.rand(nb_samples2, latent_dim[1])
+        init = [Z1init, Z2init]
 
-        som_numpy.fit(nb_epoch=nb_epoch)
-        som_use_for.fit(nb_epoch=nb_epoch)
+        # generate tsom instance
+        tsom_numpy = TSOM2(X, latent_dim=latent_dim, resolution=resolution,
+                           SIGMA_MAX=sigma_max, SIGMA_MIN=sigma_min, TAU=tau,
+                           init=init)
+        tsom_use_for = TSOM2UseFor(X, latent_dim=latent_dim, resolution=resolution,
+                                   SIGMA_MAX=sigma_max, SIGMA_MIN=sigma_min, TAU=tau,
+                                   init=init)
 
-        np.testing.assert_allclose(som_numpy.history['y'],som_use_for.history['y'])
+        # learn
+        tsom_numpy.fit(nb_epoch=nb_epoch)
+        tsom_use_for.fit(nb_epoch=nb_epoch)
+
+        # test
+        np.testing.assert_allclose(tsom_numpy.history['y'], tsom_use_for.history['y'])
+        np.testing.assert_allclose(tsom_numpy.history['z1'], tsom_use_for.history['z1'])
+        np.testing.assert_allclose(tsom_numpy.history['z2'], tsom_use_for.history['z2'])
+
 
 if __name__ == "__main__":
     unittest.main()
