f9b6c7e2d71580b13c84edbf7496f2376e80550b
町田雄紀 <machida_yuki@machidayuuki-no-MacBook-Air.local>
2018-10-12 23:05:21 +0900

    TSOMの勝者位置k_starを外部から参照できるようにself化しましたmachida

--- libs/models/tsom.py
+++ libs/models/tsom.py
@@ -136,15 +136,3 @@ class TSOM2():
             self.history['z2'][epoch, :] = self.Z2
             self.history['sigma1'][epoch] = sigma1
             self.history['sigma2'][epoch] = sigma2
-
-if __name__ == '__main__':
-    from libs.datasets.real.beverage import load_data
-    from libs.visualization.tsom.tsom2_viewer import TSOM2_Viewer as TSOM2_V
-    objects=load_data()
-    print(len(objects))
-    X=objects[0]
-    tsom = TSOM2(X,latent_dim=2,resolution=10,SIGMA_MAX=2.0,SIGMA_MIN=0.2,TAU=50)
-    tsom.fit(nb_epoch=10)
-    print(tsom.X.shape,tsom.Y.shape)
-    comp=TSOM2_V(x=tsom.X,y=tsom.Y,winner1=tsom.k_star1,winner2=tsom.k_star2)
-    comp.draw_map()
