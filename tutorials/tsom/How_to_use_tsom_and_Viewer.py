024cafbaa5144eb6330b361320c958632d5224c1
watanabe_ryuji <watanabe-ryuji@edu.brain.kyutech.ac.jp>
2018-10-19 16:01:49 +0900

    チュートリアルコードの学習回数の変更

--- tutorials/tsom/How_to_use_tsom_and_Viewer.py
+++ tutorials/tsom/How_to_use_tsom_and_Viewer.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     X,beverage,situation=load_data(ret_beverage_label=True,ret_situation_label=True)
 
     tsom = TSOM2(X,latent_dim=2,resolution=10,SIGMA_MAX=2.0,SIGMA_MIN=0.2,TAU=50)
-    tsom.fit(nb_epoch=3)
-    comp=TSOM2_V(y=tsom.Y,winner1=tsom.k_star1,winner2=tsom.k_star2,label1=np.arange(X.shape[0]),label2=beverage,button_label=situation)
+    tsom.fit(nb_epoch=50)
+    comp=TSOM2_V(y=tsom.Y,winner1=tsom.k_star1,winner2=tsom.k_star2,label1=None,label2=beverage,button_label=situation)
     comp.draw_map()
