eab735b82989d6e1cd4dbf327632e2171b249b1c
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2018-12-07 18:32:38 +0900

    delete __calc_umatrix, edit to use ...tools/calc_umatrix, reformat code

--- libs/visualization/som/Umatrix.py
+++ libs/visualization/som/Umatrix.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import scipy.spatial.distance as dist
 from sklearn.preprocessing import StandardScaler
 import matplotlib.animation
+from ...tools.calc_umatrix import calc_umatrix
+
 
 class SOM_Umatrix:
     def __init__(self, X=None, Z=None, sigma=0.2, resolution=100,
-                 labels=None, fig_size=[6,6], title_text='U-matrix', cmap_type='jet',
+                 labels=None, fig_size=[6, 6], title_text='U-matrix', cmap_type='jet',
                  interpolation_method='spline36', repeat=False, interval=40):
         # インプットが無効だった時のエラー処理
         if Z is None:
@@ -20,23 +22,23 @@ class SOM_Umatrix:
 
         # set latent variables
         if Z.ndim == 3:
-            self.Z_allepoch = Z     # shape=(T,N,L)
+            self.Z_allepoch = Z  # shape=(T,N,L)
             self.isStillImage = False
         elif Z.ndim == 2:
-            self.Z_allepoch = Z[None,:,:] # shape=(1,N,L)
+            self.Z_allepoch = Z[None, :, :]  # shape=(1,N,L)
             self.isStillImage = True
 
         # set sigma
         if isinstance(sigma, (int, float)):
             if self.isStillImage:
-                self.sigma_allepoch = np.array([sigma,])
+                self.sigma_allepoch = np.array([sigma, ])
             else:
                 raise ValueError("if Z is 3d array, sigma must be 1d array")
         elif sigma.ndim == 1:
             if self.isStillImage:
                 raise ValueError("if Z is 2d array, sigma must be scalar")
             else:
-                self.sigma_allepoch = sigma #shape=(T)
+                self.sigma_allepoch = sigma  # shape=(T)
         else:
             raise ValueError("sigma must be 1d array or scalar")
 
@@ -68,32 +70,31 @@ class SOM_Umatrix:
         self.interval = interval
 
         # 潜在空間の代表点の設定
-        self.Zeta = np.meshgrid(np.linspace(self.Z_allepoch[:, :, 0].min(), self.Z_allepoch[:, :, 0].max(), self.resolution),
-                                np.linspace(self.Z_allepoch[:, :, 1].min(), self.Z_allepoch[:, :, 1].max(), self.resolution))
+        self.Zeta = np.meshgrid(
+            np.linspace(self.Z_allepoch[:, :, 0].min(), self.Z_allepoch[:, :, 0].max(), self.resolution),
+            np.linspace(self.Z_allepoch[:, :, 1].min(), self.Z_allepoch[:, :, 1].max(), self.resolution))
         self.Zeta = np.dstack(self.Zeta).reshape(self.K, self.L)
-
 
     # U-matrix表示
     def draw_umatrix(self):
 
         # U-matrixの初期状態を表示する
-        Z = self.Z_allepoch[0,:,:]
+        Z = self.Z_allepoch[0, :, :]
         sigma = self.sigma_allepoch[0]
 
         # U-matrix表示用の値を算出
-        dY_std = self.__calc_umatrix(Z,sigma)
+        dY_std = calc_umatrix(Zeta=self.Zeta, Z=Z, X=self.X, sigma=sigma) # return value in [-2.0,2.0]
         U_matrix_val = dY_std.reshape((self.resolution, self.resolution))
 
         # U-matrix表示
         self.Map.set_title(self.title_text)
         self.Im = self.Map.imshow(U_matrix_val, interpolation=self.interpolation_method,
-                      extent=[self.Zeta[:, 0].min(), self.Zeta[:, 0].max(),
-                              self.Zeta[:, 1].max(), self.Zeta[:, 1].min()],
-                   cmap=self.Cmap_type, vmax=0.5, vmin=-0.5, animated=True)
+                                  extent=[self.Zeta[:, 0].min(), self.Zeta[:, 0].max(),
+                                          self.Zeta[:, 1].max(), self.Zeta[:, 1].min()],
+                                  cmap=self.Cmap_type, vmax=2.0, vmin=-2.0, animated=True)
 
         # ラベルの表示
         self.Scat = self.Map.scatter(x=Z[:, 0], y=Z[:, 1], c='k')
-
 
         # 勝者位置が重なった時用の処理
         self.Label_Texts = []
@@ -107,14 +108,14 @@ class SOM_Umatrix:
             self.Label_Texts.append(Label_Text)
 
         ani = matplotlib.animation.FuncAnimation(self.Fig, self.update, interval=self.interval, blit=False,
-                                           repeat=self.repeat, frames=self.T)
+                                                 repeat=self.repeat, frames=self.T)
         plt.show()
 
     def update(self, epoch):
-        Z = self.Z_allepoch[epoch,:,:]
+        Z = self.Z_allepoch[epoch, :, :]
         sigma = self.sigma_allepoch[epoch]
 
-        dY_std = self.__calc_umatrix(Z, sigma)
+        dY_std = calc_umatrix(Zeta=self.Zeta, Z=Z, X=self.X, sigma=sigma) # return value in [-2.0,2.0]
         U_matrix_val = dY_std.reshape((self.resolution, self.resolution))
         self.Im.set_array(U_matrix_val)
         self.Scat.set_offsets(Z)
@@ -129,28 +130,5 @@ class SOM_Umatrix:
                                                 self.labels[i],
                                                 color='k')
         if not self.isStillImage:
-            self.Map.set_title(self.title_text+' epoch={}'.format(epoch))
+            self.Map.set_title(self.title_text + ' epoch={}'.format(epoch))
 
-    # U-matrix表示用の値（勾配）を算出
-    def __calc_umatrix(self, Z, sigma):
-        # H, G, Rの算出
-        dist_z = dist.cdist(self.Zeta, Z, 'sqeuclidean')
-        H = np.exp(-dist_z / (2 * sigma * sigma))
-        G = H.sum(axis=1)[:, np.newaxis]
-        R = H / G
-
-        # V, V_meanの算出
-        V = R[:, :, np.newaxis] * (Z[np.newaxis, :, :] - self.Zeta[:, np.newaxis, :])          # KxNxL
-        V_mean = V.sum(axis=1)[:, np.newaxis, :]                                                    # Kx1xL
-
-        # dYdZの算出
-        dRdZ = V - R[:, :, np.newaxis] * V_mean                                                     # KxNxL
-        dYdZ = np.einsum("knl,nd->kld", dRdZ, self.X)     # KxLxD
-        dYdZ_norm = np.sum(dYdZ ** 2, axis=(1, 2))                                                  # Kx1
-
-        # 表示用の値を算出（標準化）
-        sc = StandardScaler()
-        dY_std = sc.fit_transform(dYdZ_norm[:, np.newaxis])
-
-
-        return np.clip(dY_std / 4.0, -0.5, 0.5)
