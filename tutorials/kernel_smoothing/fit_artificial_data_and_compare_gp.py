d9a44356747db1ffc5f3acda55204cd3bf7a6b4b
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2018-12-17 10:44:26 +0900

    Adjust tutorial code

--- tutorials/kernel_smoothing/fit_artificial_data_and_compare_gp.py
+++ tutorials/kernel_smoothing/fit_artificial_data_and_compare_gp.py
@@ -15,25 +15,26 @@ def _main():
     x = np.random.rand(nb_samples) * 2.0 - 1.0
     y = func(x) + np.random.normal(0.0, x_noise_sigma, x.shape)
 
-    # Set parameter of ks
-    sigma = 0.1
-
     # Set parameter to visualize
     nb_new_samples = 200
-    xnew = np.linspace(-1.2,1.2,nb_new_samples)
+    xnew = np.linspace(-1.1,1.1,nb_new_samples)
     f_true = func(xnew)
 
-    # Fit and predict by kernel smoothing
-    ks = KernelSmoothing(sigma=sigma)
-    ks.fit(x,y)
-    f_ks = ks.predict(xnew)
-
-    # Plot training samples and estimated function
-    fig = plt.figure(figsize=[5,3])
+    # Plot training samples
+    fig = plt.figure(figsize=[7,4])
     ax = fig.add_subplot(1,1,1)
     ax.scatter(x,y,label='samples', s=8)
-    ax.plot(xnew,f_ks,label='ks')
     ax.plot(xnew,f_true,label='true')
+
+    # Fit and predict by kernel smoothing
+    sigma_list = [0.01, 0.1, 1.0]
+    for sigma in sigma_list:
+        ks = KernelSmoothing(sigma=sigma)
+        ks.fit(x,y)
+        f_ks = ks.predict(xnew)
+
+        ax.plot(xnew,f_ks,label='$\sigma$={:.2f}'.format(sigma))
+
     plt.legend()
     plt.show()
 
