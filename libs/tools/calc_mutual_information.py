59e06e30ec389d3b89787892ca1beaa08db00fc1
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-01-23 21:14:10 +0900

    関数名，変数名の変更

--- libs/tools/calc_mutual_information.py
+++ libs/tools/calc_mutual_information.py
@@ -1,13 +1,13 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
-def calMI(Z_true, Z_estimate, nb_bins=100,bias=0.0001):
-    if Z_true.shape[1] != 1 or Z_estimate.shape[1] != 1:
+
+def calc_mutual_information(x, y, nb_bins=100, bias=0.0001):
+    if x.shape[1] != 1 or y.shape[1] != 1:
         raise ValueError('now, this function follow latent_dim = 1 only')
 
-    hist_true,bin_edges = np.histogram(Z_true.ravel(),bins=nb_bins)
-    hist_estimate,bin_edges = np.histogram(Z_estimate.ravel(),bins=nb_bins)
-    hist_joint,x_edges,y_edges = np.histogram2d(Z_true.ravel(),Z_estimate.ravel(),bins=nb_bins)
+    hist_true,bin_edges = np.histogram(x.ravel(), bins=nb_bins)
+    hist_estimate,bin_edges = np.histogram(y.ravel(), bins=nb_bins)
+    hist_joint,x_edges,y_edges = np.histogram2d(x.ravel(), y.ravel(), bins=nb_bins)
 
     hist_true = hist_true.astype(np.float32) + bias
     hist_estimate = hist_estimate.astype(np.float32) + bias
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     fig = plt.figure(figsize=[9,3])
     nb_patterns = len(list_Z_estimate)
     for i,Z_estimate in enumerate(list_Z_estimate):
-        MI = calMI(Z_true=Z_true,Z_estimate=Z_estimate,nb_bins=nb_bins)
+        MI = calc_mutual_information(x=Z_true, y=Z_estimate, nb_bins=nb_bins)
 
         ax = fig.add_subplot(1,nb_patterns,i+1)
 
