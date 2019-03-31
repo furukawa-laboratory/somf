d0d80884debd87e4c0745d1e1faea59ad3f765ad
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-03-31 22:25:57 +0900

    Add retz in arguments, refactor and add sample codes

--- libs/datasets/artificial/kura_tsom.py
+++ libs/datasets/artificial/kura_tsom.py
@@ -1,26 +1,29 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 
 #
-def load_kura_tsom(xsamples,ysamples):
+def load_kura_tsom(xsamples,ysamples,retz=False):
     x=np.linspace(-1,1,xsamples)
     y=np.linspace(-1,1,ysamples)
 
     xx,yy=np.meshgrid(y,x)
     z=xx**2-yy**2
 
-    X=np.concatenate((xx[:,:,np.newaxis],yy[:,:,np.newaxis],z[:,:,np.newaxis]),axis=2)
+    x=np.concatenate((xx[:,:,np.newaxis],yy[:,:,np.newaxis],z[:,:,np.newaxis]),axis=2)
 
 
-    return X
+    if retz:
+        return x, xx, yy
+    else:
+        return x
 
 
 
 
-
-    # fig=plt.figure()
-    # ax=Axes3D(fig)
-    # ax.scatter(X[:,:,0],X[:,:,1],X[:,:,2])
-    # plt.show()
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    fig=plt.figure()
+    ax=Axes3D(fig)
+    ax.scatter(X[:,:,0],X[:,:,1],X[:,:,2])
+    plt.show()
