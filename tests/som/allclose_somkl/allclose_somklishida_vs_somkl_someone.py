69e86cd0332a3512eb9485b3d6e2853ca9a79e84
Ishida Takuro <>
2019-04-06 15:02:19 +0900

    euclid=Trueの状態でallcloseになったことを確認

--- tests/som/allclose_somkl/allclose_somklishida_vs_somkl_someone.py
+++ tests/som/allclose_somkl/allclose_somklishida_vs_somkl_someone.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from libs.models.som import SOM
-from libs.models.som_tensorflow import SOM as som
+from  tests.som.allclose_somkl.som_someone import SOM_someone
 
 
 class TestSOM(unittest.TestCase):
@@ -22,15 +22,14 @@ class TestSOM(unittest.TestCase):
         SIGMA_MAX = 2.2
         SIGMA_MIN = 0.1
         TAU = 50
-        som_numpy = SOM(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit)
-        som_tensorflow = som(X.shape[1], X.shape[0], n=resolution, m=resolution,epochs=nb_epoch,sigma_max=SIGMA_MAX,sigma_min=SIGMA_MIN,tau=TAU,init=Zinit)
+        som_ishida = SOM(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit)
+        som_someone = SOM_someone(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit)
 
-        som_numpy.fit(nb_epoch=nb_epoch)
-        som_tensorflow.predict(X)
+        som_ishida.fit(nb_epoch=nb_epoch,euclid=True)
+        som_someone.fit(nb_epoch=nb_epoch,euclid=True)
 
 
-
-        np.testing.assert_allclose(som_numpy.history['y'],som_tensorflow.historyY, atol=1e-06)
+        np.testing.assert_allclose(som_ishida.history['y'],som_someone.history['y'])
 
 
 
