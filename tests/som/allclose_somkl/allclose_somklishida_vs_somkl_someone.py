ab6732cd4a46ea7a6d6e363814c7a13254d144ef
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-04-07 21:22:35 +0900

    Edit test code

--- tests/som/allclose_somkl/allclose_somklishida_vs_somkl_someone.py
+++ tests/som/allclose_somkl/allclose_somklishida_vs_somkl_someone.py
@@ -3,11 +3,11 @@ import unittest
 import numpy as np
 
 from libs.models.som import SOM
-from  tests.som.allclose_somkl.som_someone import SOM_someone
+from tests.som.allclose_somkl.som_watanabe import SOM_watanabe
 
 
 class TestSOM(unittest.TestCase):
-    def test_numpy_vs_usefor(self):
+    def test_kldiv_ishida_vs_watanabe(self):
         N = 100
         D = 3
         L = 2
@@ -19,19 +19,20 @@ class TestSOM(unittest.TestCase):
         X_sum=np.sum(X0,axis=1)
         X=X0/X_sum[:,np.newaxis]
         Zinit = np.random.rand(N,L)*2.0 -1.0
+        metric = "KLdivergence"
 
         nb_epoch = 200
         SIGMA_MAX = 2.2
         SIGMA_MIN = 0.1
         TAU = 50
-        som_ishida = SOM(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit,metric="sqeuclidean")
-        som_someone = SOM_someone(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit,metric="sqeuclidean")
+        som_ishida = SOM(X,L,resolution,SIGMA_MAX,SIGMA_MIN,TAU,init=Zinit,metric=metric)
+        som_watanabe = SOM_watanabe(X, L, resolution, SIGMA_MAX, SIGMA_MIN, TAU, init=Zinit, metric=metric)
 
         som_ishida.fit(nb_epoch=nb_epoch)
-        som_someone.fit(nb_epoch=nb_epoch)
+        som_watanabe.fit(nb_epoch=nb_epoch)
 
 
-        np.testing.assert_allclose(som_ishida.history['y'],som_someone.history['y'])
+        np.testing.assert_allclose(som_ishida.history['y'],som_watanabe.history['y'])
 
 
 
