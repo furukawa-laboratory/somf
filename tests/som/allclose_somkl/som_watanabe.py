Fix calculating KL divergence

tests/som/allclose_somkl/som_watanabe.py
@@ -86,7 +86,7 @@ class SOM_watanabe:
                 # argmin(axis=1)を用いて各行で最小値を探しそのインデックスを返す
                 self.Z = self.Zeta[bmus, :]  # 勝者ノード番号から勝者ノードを求める
             elif self.metric is "KLdivergence":  # KL情報量を使った勝者決定
-                Dist = np.sum(self.X[:,None,:] * self.Y[None,:,:],axis=2)
+                Dist = np.sum(self.X[:,None,:] * np.log(self.Y[None,:,:]),axis=2)
                 bmus = Dist.argmax(axis=1)
                 self.Z = self.Zeta[bmus, :]
 