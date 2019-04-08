1ca1d7f0c16044f6a3e60adcf81360992d013c88
Ishida Takuro <>
2019-04-08 14:06:30 +0900

    main関数で下位のTSOMのfitまで実装(エラーはなし)

--- libs/models/TSOM_plus_SOM.py
+++ libs/models/TSOM_plus_SOM.py
@@ -4,8 +4,8 @@ from libs.models.som import SOM
 import numpy as np
 
 class TSOM_plus_SOM:
-    def __init__(self,input_data,kernel_width,tsom_latentdim,tsom_resolution,tsom_sigma_max,tsom_sigma_min,tsom_tau,tsom_epoch_num,
-                 som_latentdim,som_resolution,som_sigma_max,,som_sigma_min,som_tau):
+    def __init__(self,input_data,tsom_latentdim,tsom_resolution,tsom_sigma_max,tsom_sigma_min,tsom_tau,tsom_epoch_num,
+                 som_latentdim,som_resolution,som_sigma_max,som_sigma_min,som_tau):
 
         #下位のTSOMのパラメータ設定(とりあえず、全モード共通)
         self.tsom_latent_dim=tsom_latentdim
@@ -13,7 +13,6 @@ class TSOM_plus_SOM:
         self.tsom_sigma_min=tsom_sigma_min
         self.tsom_tau=tsom_tau
         self.tsom_resolution=tsom_resolution
-        self.tsom_epoch_num = tsom_epoch_num
         #上位のSOMのパラメータ設定
         self.som_latent_dim=som_latentdim
         self.som_sigma_max = som_sigma_max
@@ -24,7 +23,7 @@ class TSOM_plus_SOM:
 
 
         self.input_data=input_data#下位のTSOMに入れるパラメータ
-        self.kernel_width=kernel_width#KDEのカーネル幅を決める(ガウスカーネル)
+        #self.kernel_width=kernel_width#KDEのカーネル幅を決める(ガウスカーネル)
 
         #上位のSOMのパラメータ設定と、下位TSOMのパラメータ設定を引数として決めてアyる必要がある.
         self.tsom=TSOM2(self.input_data,latent_dim=self.tsom_latent_dim,resolution=self.tsom_resolution,SIGMA_MAX=self.tsom_sigma_max
@@ -33,11 +32,12 @@ class TSOM_plus_SOM:
         #いるやつ:TSOMクラス,SOMクラス,KDE関数
 
 
-    def fit_1st_TSOM(self):
-        self.tsom.fit(self.tsom_epoch_num)
+    def fit_1st_TSOM(self,tsom_epoch_num):
+        self.tsom.fit(tsom_epoch_num)
 
-    def fit_KDE(self):#学習した後の潜在空間からKDEで確率分布を作る
+    def fit_KDE(self,kernel_width,group_label):#学習した後の潜在空間からKDEで確率分布を作る
         pass
+        #グループ情報とカーネル幅を入れる
 
     def fit_2nd_SOM(self):#上位のSOMを
         pass
@@ -47,10 +47,23 @@ def _main():
     N=10
     D=20
     input_data=np.random.rand(N,D)
-    firstTSOM_parameters=1.0
-    secondSOM_parameters=1.0
-    htsom=TSOM_plus_SOM(input_data,kernel_width=1.0,firstTSOM_parameters=firstTSOM_parameters,secondSOM_parameters=secondSOM_parameters)
+    kernel_width1=1.0
+    tsom_latentdim=2
+    tsom_resolution=10
+    tsom_sigma_max=1.0
+    tsom_sigma_min=0.1
+    tsom_tau=50
+    tsom_epoch_num=250
+    som_latentdim=2
+    som_resolution=10
+    som_sigma_max=1.0
+    som_sigma_min=0.1
+    som_tau=50
+    htsom=TSOM_plus_SOM(input_data,tsom_latentdim,tsom_resolution,tsom_sigma_max,tsom_sigma_min,tsom_tau,tsom_epoch_num,
+                        som_latentdim,som_resolution,som_sigma_max,som_sigma_min,som_tau)
     #htsom内で呼び出しているtsomのクラス内の変数を参照できるか？→selfつければできるよ！
+    htsom.fit_1st_TSOM(tsom_epoch_num=250)
+
 
 if __name__ == '__main__':
     _main()
