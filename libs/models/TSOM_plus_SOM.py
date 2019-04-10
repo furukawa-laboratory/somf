4541fa124dec78b25207087bec8c413845b44fc9
Ishida Takuro <>
2019-04-10 16:47:52 +0900

    group_labelをinitに入れた

--- libs/models/TSOM_plus_SOM.py
+++ libs/models/TSOM_plus_SOM.py
@@ -5,9 +5,9 @@ import numpy as np
 from scipy.spatial import distance as dist
 
 class TSOM_plus_SOM:
-    def __init__(self,input_data,**kargs):
-        #とりあえず、keyは固定にして場所自由でいいかも.
-        #下位のTSOMのパラメータ設定(とりあえず、全モード共通)
+    def __init__(self,input_data,group_label,**kargs):
+        #とりあえず、keyは固定にして場所自由でいいかも.一部、tsomの時にtupleになっている場合の処理を追加
+        #下位のTSOMのパラメータ設定
         self.tsom_latent_dim=kargs['tsom_latentdim']
         self.tsom_resolution = kargs['tsom_resolution']
         self.tsom_sigma_max=kargs['tsom_sigma_max']
@@ -22,22 +22,22 @@ class TSOM_plus_SOM:
         self.som_resolution = kargs['som_resolution']
 
         self.input_data=input_data#下位のTSOMに入れるパラメータ
+        self.group_label = group_label # グループ数の確認
+        self.group_num=len(self.group_label)
 
         #上位のSOMのパラメータ設定と、下位TSOMのパラメータ設定を引数として決めてアyる必要がある.
         self.tsom=TSOM2(self.input_data,latent_dim=self.tsom_latent_dim,resolution=self.tsom_resolution,SIGMA_MAX=self.tsom_sigma_max
                         ,SIGMA_MIN=self.tsom_sigma_min,init='random',TAU=self.tsom_tau)
-
+        self.output_data = np.zeros((self.group_num, self.tsom.K1))  # group数*ノード数
 
 
     def fit_1st_TSOM(self,tsom_epoch_num):
         self.tsom.fit(tsom_epoch_num)
 
-    def fit_KDE(self,kernel_width,group_label):#学習した後の潜在空間からKDEで確率分布を作る
-        group_num=len(group_label)#グループ数の確認
-        self.output_data=np.zeros((group_num,self.tsom.K1))#group数*ノード数
+    def fit_KDE(self,kernel_width):#学習した後の潜在空間からKDEで確率分布を作る
         #グループごとにKDEを適用
-        for i in range(group_num):
-            Dist=dist.cdist(self.tsom.Zeta1, self.tsom.Z1[group_label[i],:], 'sqeuclidean')# KxNの距離行列を計算
+        for i in range(self.group_num):
+            Dist=dist.cdist(self.tsom.Zeta1, self.tsom.Z1[self.group_label[i],:], 'sqeuclidean')# KxNの距離行列を計算
             H = np.exp(-Dist / (2 * kernel_width * kernel_width))  # KxNの学習量行列を計算
             prob = np.sum(H, axis=1)#K*1
             prob_sum = np.sum(prob)#1*1
@@ -67,17 +67,14 @@ def _main():
     group1_label=np.arange(group1_data.shape[0])
     group2_label = np.arange(group2_data.shape[0])+group1_data.shape[0]
     group3_label=np.arange(group3_data.shape[0])+group1_data.shape[0]+group2_data.shape[0]
-    print(group1_label)
-    print(group2_label)
-    print(group3_label)
     group_label=(group1_label,group2_label,group3_label)
 
     #dictのパラメータ名は固定
-    htsom=TSOM_plus_SOM(input_data=input_data,tsom_resolution=10,tsom_latentdim=2 ,tsom_sigma_max=1.0, tsom_sigma_min=0.1, tsom_tau=50,
-                        som_latentdim=2,som_resolution=10,som_sigma_max=1.0,som_sigma_min=0.1,som_tau=50)
+    htsom=TSOM_plus_SOM(input_data=input_data,group_label=group_label,tsom_resolution=10,tsom_latentdim=2 ,tsom_sigma_max=1.0,
+                        tsom_sigma_min=0.1,tsom_tau=50,som_latentdim=2,som_resolution=10,som_sigma_max=1.0,som_sigma_min=0.1,som_tau=50)
     #htsom内で呼び出しているtsomのクラス内の変数を参照できるか？→selfつければできるよ！
     htsom.fit_1st_TSOM(tsom_epoch_num=250)
-    htsom.fit_KDE(kernel_width=1.0,group_label=group_label)
+    htsom.fit_KDE(kernel_width=1.0)
     htsom.fit_2nd_SOM(som_epoch_num=250)
 
 if __name__ == '__main__':
