968035e8e683827c27665bee504e4a128b41c031
Ishida Takuro <>
2019-04-23 12:03:23 +0900

    KDEまであっていることは確認

--- tests/plus_TSOM/allclose_plusTSOM.py
+++ tests/plus_TSOM/allclose_plusTSOM.py
@@ -13,8 +13,10 @@ class TestSOM(unittest.TestCase):
         seed = 100
         np.random.seed(seed)
         #初期値を合わせる
-        Z1_init=np.random.rand((group_num * samples_per_group,input_dim))
-        Z2_init = np.random.rand((group_num * samples_per_group, input_dim))
+        Z1 = np.random.rand(group_num * samples_per_group, 2) * 2.0 - 1.0
+        Z2 = np.random.rand(input_dim, 2) * 2.0 - 1.0
+        init = (Z1, Z2)
+
 
         #学習データの作成(平均のみが違うガウス分布からサンプリングした人工データを用いる.ガウス分布数がグループ数で,サンプル数がメンバーの数)
         # 平均ベクトルを一様分布から生成
@@ -36,8 +38,8 @@ class TestSOM(unittest.TestCase):
 
         # #plus型TSOMの学習
         #dictのパラメータ名は固定latent_dim,resolution,sigma_max,sigma_min,tauでSOMとTSOMでまとめる
-        htsom_ishida = TSOM_plus_SOM(input_data, group_label, ((2, 1), 2), ((5, 10), 10), (1.0, 1.0), (0.1, 0.1), (50, 50))
-        htsom_someone=TSOM_plus_SOM_someone(input_data, group_label, ((2, 1), 2), ((5, 10), 10), (1.0, 1.0), (0.1, 0.1), (50, 50))
+        htsom_ishida = TSOM_plus_SOM(input_data,init,group_label, ((2, 2), 2), ((5, 10), 10), (1.0, 1.0), (0.1, 0.1), (50, 50))
+        htsom_someone=TSOM_plus_SOM_someone(input_data,init ,group_label, ((2, 2), 2), ((5, 10), 10), (1.0, 1.0), (0.1, 0.1), (50, 50))
 
 
         # plus型TSOM(TSOM*SOM)のやつ
@@ -50,19 +52,17 @@ class TestSOM(unittest.TestCase):
         np.testing.assert_allclose(htsom_ishida.tsom.history['z2'], htsom_someone.tsom.history['z2'])
 
 
-        # htsom_ishida.fit_KDE(kernel_width=1.0)  # カーネル密度推定を使って2ndSOMに渡す確率分布を作成
-        # htsom_someone.fit_KDE(kernel_width=1.0)  # カーネル密度推定を使って2ndSOMに渡す確率分布を作成
-        # #print("allclose_Kernel_Dnsity_Estimation")
-        # #np.testing.assert_allclose(htsom_ishida.prob_data, htsom_someone.prob_data)
-        #
-        # htsom_ishida.fit_2nd_SOM(som_epoch_num=250)  # 2ndSOMの学習
-        # htsom_someone.fit_2nd_SOM(som_epoch_num=250)  # 2ndSOMの学習
+        htsom_ishida.fit_KDE(kernel_width=1.0)  # カーネル密度推定を使って2ndSOMに渡す確率分布を作成
+        htsom_someone.fit_KDE(kernel_width=1.0)  # カーネル密度推定を使って2ndSOMに渡す確率分布を作成
+        print("allclose_Kernel_Dnsity_Estimation")
+        np.testing.assert_allclose(htsom_ishida.prob_data, htsom_someone.prob_data)
 
-       # print("allclose_2ndSOM")
-        #np.testing.assert_allclose(htsom_ishida.som.history['y'], htsom_someone.som.history['y'])
-        #np.testing.assert_allclose(htsom_ishida.som.history['z'], htsom_someone.som.history['z'])
+        htsom_ishida.fit_2nd_SOM(som_epoch_num=250)  # 2ndSOMの学習
+        htsom_someone.fit_2nd_SOM(som_epoch_num=250)  # 2ndSOMの学習
 
-        #np.testing.assert_allclose(som_ishida.history['y'],som_watanabe.history['y'])
+        print("allclose_2ndSOM")
+        np.testing.assert_allclose(htsom_ishida.som.history['y'], htsom_someone.som.history['y'])
+        np.testing.assert_allclose(htsom_ishida.som.history['z'], htsom_someone.som.history['z'])
 
 
 
