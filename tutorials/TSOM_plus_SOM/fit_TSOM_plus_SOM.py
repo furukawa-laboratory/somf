58d246e1d0ec1159e9ebfe5f3a1fc5d25c7ca717
Ishida Takuro <>
2019-09-13 22:52:21 +0900

    plus型TSOMのチュートリアルコードの修正

--- tutorials/TSOM_plus_SOM/fit_TSOM_plus_SOM.py
+++ tutorials/TSOM_plus_SOM/fit_TSOM_plus_SOM.py
@@ -6,30 +6,30 @@ from libs.models.TSOM_plus_SOM import TSOM_plus_SOM
 from mpl_toolkits.mplot3d import Axes3D
 from libs.visualization.som.Grad_norm import Grad_Norm
 
-# #人工データの検証
-# xsamples=80#x_samplesでメンバーの人数が100人になるように調整
-# ysamples=20
-# X,z=load_kura_tsom(xsamples=xsamples,ysamples=ysamples,retz=True)
-#
-# #チームを分割(偶数チームのみ)
+#人工データの検証
+xsamples=40#x_samplesでメンバーの人数が100人になるように調整
+ysamples=20
+X,z=load_kura_tsom(xsamples=xsamples,ysamples=ysamples,retz=True)
+
+#チームを分割(偶数チームのみ)
 group_num=8
-# input_data=np.zeros((group_num,int(xsamples*ysamples/group_num),3))#グループ数*メンバー数*次元
-# #観測データを分割
-# for i in range(int(group_num/2)):
-#     group1 = X[int(xsamples*i /int(group_num/2)):int(xsamples*(i+1) / int(group_num/2)), 0:int(ysamples / 2), :]
-#     group1=group1.reshape((int(xsamples*ysamples/group_num),3))
-#     group2 = X[int(xsamples*i / int(group_num/2)):int(xsamples*(i+1) / int(group_num/2)), int(ysamples / 2):int(ysamples), :]
-#     group2=group2.reshape((int(xsamples*ysamples/group_num),3))
-#
-#     input_data[int(2*i),:,:]=group1
-#     input_data[int(2*i+1), :, :] = group2
-#
-#
-# fig=plt.figure()
-# ax=fig.add_subplot(1,1,1,projection="3d")
-# for i in range(group_num):
-#     ax.scatter(input_data[i,:,0], input_data[i,:,1], input_data[i,:,2])
-# plt.show()
+input_data=np.zeros((group_num,int(xsamples*ysamples/group_num),3))#グループ数*メンバー数*次元
+#観測データを分割
+for i in range(int(group_num/2)):
+    group1 = X[int(xsamples*i /int(group_num/2)):int(xsamples*(i+1) / int(group_num/2)), 0:int(ysamples / 2), :]
+    group1=group1.reshape((int(xsamples*ysamples/group_num),3))
+    group2 = X[int(xsamples*i / int(group_num/2)):int(xsamples*(i+1) / int(group_num/2)), int(ysamples / 2):int(ysamples), :]
+    group2=group2.reshape((int(xsamples*ysamples/group_num),3))
+
+    input_data[int(2*i),:,:]=group1
+    input_data[int(2*i+1), :, :] = group2
+
+
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1,projection="3d")
+for i in range(group_num):
+    ax.scatter(input_data[i,:,0], input_data[i,:,1], input_data[i,:,2])
+plt.show()
 
 #グループラベルの作成
 group_label=[]
@@ -37,17 +37,19 @@ for i in range(group_num):
     group_label_i=np.arange(int(100*(i)),int(100*(i+1)))
     group_label.append(group_label_i)
 
-# args=((1,1),(10,10),(1.0,1.0),(0.1,0.1),(50,50))
-#
-# #+型階層TSOMのclass読み込み
-# # group_label以降の変数ははlatent_dim,resolution,sigma_max,sigma_min,tauでSOMとTSOMでまとめている
-# tsom_plus_som=TSOM_plus_SOM(input_data,"random",group_label,(2,2),(10,10),(1.0,1.0),(0.1,0.1),(50,50))
-#
-# tsom_plus_som.fit_1st_TSOM(tsom_epoch_num=250)
-# tsom_plus_som.fit_KDE(kernel_width=1.0)
-# tsom_plus_som.fit_2nd_SOM(som_epoch_num=250,init="random")#2ndSOMの学習
-#
-#
-# #grad_normでteamを可視化
-# som_umatrix = Grad_Norm(X=tsom_plus_som.som.X,Z=tsom_plus_som.som.Z,sigma=0.1,labels=None,title_text="team_map",resolution=20)
-# som_umatrix.draw_umatrix()
+
+
+args=((1,1),(10,10),(1.0,1.0),(0.1,0.1),(50,50))
+
+#+型階層TSOMのclass読み込み
+# group_label以降の変数ははlatent_dim,resolution,sigma_max,sigma_min,tauでSOMとTSOMでまとめている
+tsom_plus_som=TSOM_plus_SOM(input_data,"random",group_label,(2,2),(10,10),(1.0,1.0),(0.1,0.1),(50,50))
+
+tsom_plus_som.fit_1st_TSOM(tsom_epoch_num=250)
+tsom_plus_som.fit_KDE(kernel_width=1.0)
+tsom_plus_som.fit_2nd_SOM(som_epoch_num=250,init="random")#2ndSOMの学習
+
+
+#grad_normでteamを可視化
+som_umatrix = Grad_Norm(X=tsom_plus_som.som.X,Z=tsom_plus_som.som.Z,sigma=0.1,labels=None,title_text="team_map",resolution=10)
+som_umatrix.draw_umatrix()
