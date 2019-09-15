619c4c9e1430e7426f174f65c6af15e87d36776e
Ishida Takuro <>
2019-09-15 20:12:15 +0900

    必要のない部分を削除

--- libs/models/tsom3.py
+++ libs/models/tsom3.py
@@ -1,9 +1,7 @@
 import numpy as np
 from libs.tools.create_zeta import create_zeta
-from libs.datasets.real.beverage import load_data
 from scipy.spatial import distance as distance
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
 class TSOM3():
     def __init__(self, X, latent_dim, resolution, SIGMA_MAX, SIGMA_MIN, TAU, init='random'):
@@ -181,30 +179,3 @@ class TSOM3():
             self.history['sigma1'][epoch] = sigma1
             self.history['sigma2'][epoch] = sigma2
             self.history['sigma3'][epoch] = sigma3
-
-
-def _main():
-    # データのimport
-    data_set = load_data(ret_situation_label=True, ret_beverage_label=True)
-    X = data_set[0]
-    beverage_label = data_set[1]
-    situation_label = data_set[2]
-
-    tsom3=TSOM3(X, latent_dim=(2,2,2), resolution=(5,5,5), SIGMA_MAX=(0.5,1.5,1.5), SIGMA_MIN=(0.1,0.2,0.2), TAU=(50,50,50), init='random')
-    tsom3.fit(nb_epoch=250)
-
-    #結果の描画
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 2, 1)
-    ax.scatter(tsom3.Z2[:, 0], tsom3.Z2[:, 1])
-    for i in range(X.shape[1]):
-        ax.text(tsom3.Z2[i, 0], tsom3.Z2[i, 1], beverage_label[i])
-
-    ax2 = fig.add_subplot(1, 2, 2)
-    ax2.scatter(tsom3.Z3[:, 0], tsom3.Z3[:, 1])
-    for i in range(X.shape[2]):
-        ax2.text(tsom3.Z3[i, 0], tsom3.Z3[i, 1], situation_label[i])
-    plt.show()
-
-if __name__ == "__main__":
-    _main()
