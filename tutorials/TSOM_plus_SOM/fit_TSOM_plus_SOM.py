1dfcadc726be4d8f54a051effb8c374907076ce7
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-10-30 19:31:20 +0900

    tutorial codeをモデルの新しい引数仕様に変更

--- tutorials/TSOM_plus_SOM/fit_TSOM_plus_SOM.py
+++ tutorials/TSOM_plus_SOM/fit_TSOM_plus_SOM.py
@@ -26,11 +26,11 @@ for i in range(int(group_num / 2)):
     input_data[int(2 * i), :, :] = group1
     input_data[int(2 * i + 1), :, :] = group2
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection="3d")
-for i in range(group_num):
-    ax.scatter(input_data[i, :, 0], input_data[i, :, 1], input_data[i, :, 2])
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1, projection="3d")
+# for i in range(group_num):
+#     ax.scatter(input_data[i, :, 0], input_data[i, :, 1], input_data[i, :, 2])
+# plt.show()
 
 # グループラベルの作成
 group_label = []
@@ -38,15 +38,27 @@ for i in range(group_num):
     group_label_i = np.arange(int(100 * (i)), int(100 * (i + 1)))
     group_label.append(group_label_i)
 
-args = ((1, 1), (10, 10), (1.0, 1.0), (0.1, 0.1), (50, 50))
+params_tsom = {'latent_dim':[2,2],
+               'resolution':[10,10],
+               'SIGMA_MAX':[1.0,1.0],
+               'SIGMA_MIN':[0.1,0.1],
+               'TAU':[50,50]}
+params_som = {'latent_dim':2,
+              'resolution':10,
+              'sigma_max':2.0,
+              'sigma_min':0.5,
+              'tau':50}
 
 # +型階層TSOMのclass読み込み
 # group_label以降の変数ははlatent_dim,resolution,sigma_max,sigma_min,tauでSOMとTSOMでまとめている
-tsom_plus_som = TSOM_plus_SOM(input_data, "random", group_label, (2, 2), (10, 10), (1.0, 1.0), (0.1, 0.1), (50, 50))
+tsom_plus_som = TSOM_plus_SOM(input_data=input_data,
+                              group_label=group_label,
+                              params_tsom=params_tsom,
+                              params_som=params_som)
 
 tsom_plus_som.fit_1st_TSOM(tsom_epoch_num=250)
 tsom_plus_som.fit_KDE(kernel_width=1.0)
-tsom_plus_som.fit_2nd_SOM(som_epoch_num=250, init="random")  # 2ndSOMの学習
+tsom_plus_som.fit_2nd_SOM(som_epoch_num=250)  # 2ndSOMの学習
 
 # grad_normでteamを可視化
 som_umatrix = Grad_Norm(X=tsom_plus_som.som.X, Z=tsom_plus_som.som.Z, sigma=0.1, labels=None, title_text="team_map",
