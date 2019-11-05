6e9e2bc117d0ad529331c7342295ee19e6d47378
kusumoto <kusumoto.keisuke515@mail.kyutech.jp>
2019-11-06 02:57:04 +0900

    欠損値対応TSOMをとりあえず作成．作成中に使用した jyupyter file も同階層に保存．

--- libs/models/tsom_missing_value.py
+++ libs/models/tsom_missing_value.py
@@ -22,6 +22,15 @@ class TSOM2():
         else:
             raise ValueError("invalid X: {}\nX must be 2d or 3d ndarray".format(X))
 
+        # 欠損値判定
+        self.hantei_X = np.any(np.isnan(self.X))
+        # 欠損値がある場合
+        if self.hantei_X == 1:
+            self.Gamma = np.where(np.isnan(self.X) == 1, 0, 1)
+            # X の欠損値を 0 で置換
+            self.gamma = self.Gamma[:,:,0]
+            self.X[np.isnan(self.X)] = 0
+
         # 最大近傍半径(SIGMAX)の設定
         if type(SIGMA_MAX) is float:
             self.SIGMA1_MAX = SIGMA_MAX
@@ -98,8 +107,6 @@ class TSOM2():
         else:
             raise ValueError("invalid inits: {}".format(init))
 
-
-
         self.history = {}
 
     def fit(self, nb_epoch=200):
@@ -109,33 +116,70 @@ class TSOM2():
         self.history['sigma1'] = np.zeros(nb_epoch)
         self.history['sigma2'] = np.zeros(nb_epoch)
 
-        for epoch in tqdm(np.arange(nb_epoch)):
-            # 学習量の決定
-            # sigma1 = self.SIGMA1_MIN + (self.SIGMA1_MAX - self.SIGMA1_MIN) * np.exp(-epoch / self.TAU1)
-            sigma1 = max(self.SIGMA1_MIN, self.SIGMA1_MAX * (1 - (epoch / self.TAU1)))
-            distance1 = distance.cdist(self.Zeta1, self.Z1, 'sqeuclidean')  # 距離行列をつくるDはN*K行列
-            H1 = np.exp(-distance1 / (2 * pow(sigma1, 2)))  # かっこに気を付ける
-            G1 = np.sum(H1, axis=1)  # Gは行ごとの和をとったベクトル
-            R1 = (H1.T / G1).T  # 行列の計算なので.Tで転置を行う
+        if self.hantei_X == 1:
+            for epoch in tqdm(np.arange(nb_epoch)):
+                # 学習量の決定
+                # sigma1 = self.SIGMA1_MIN + (self.SIGMA1_MAX - self.SIGMA1_MIN) * np.exp(-epoch / self.TAU1)
+                sigma1 = max(self.SIGMA1_MIN, self.SIGMA1_MAX * (1 - (epoch / self.TAU1)))
+                distance1 = distance.cdist(self.Zeta1, self.Z1, 'sqeuclidean')  # 距離行列をつくるDはN*K行列
+                H1 = np.exp(-distance1 / (2 * pow(sigma1, 2)))  # かっこに気を付ける
+                # G1 = np.sum(H1, axis=1)  # Gは行ごとの和をとったベクトル
+                # R1 = (H1.T / G1).T  # 行列の計算なので.Tで転置を行う
 
-            # sigma2 = self.SIGMA2_MIN + (self.SIGMA2_MAX - self.SIGMA2_MIN) * np.exp(-epoch / self.TAU2)
-            sigma2 = max(self.SIGMA2_MIN, self.SIGMA2_MAX * (1 - (epoch / self.TAU2)))
-            distance2 = distance.cdist(self.Zeta2, self.Z2, 'sqeuclidean')  # 距離行列をつくるDはN*K行列
-            H2 = np.exp(-distance2 / (2 * pow(sigma2, 2)))  # かっこに気を付ける
-            G2 = np.sum(H2, axis=1)  # Gは行ごとの和をとったベクトル
-            R2 = (H2.T / G2).T  # 行列の計算なので.Tで転置を行う
-            # １次モデル，２次モデルの決定
-            self.U = np.einsum('lj,ijd->ild', R2, self.X)
-            self.V = np.einsum('ki,ijd->kjd', R1, self.X)
-            self.Y = np.einsum('ki,lj,ijd->kld', R1, R2, self.X)
-            # 勝者決定
-            self.k_star1 = np.argmin(np.sum(np.square(self.U[:, None, :, :] - self.Y[None, :, :, :]), axis=(2, 3)), axis=1)
-            self.k_star2 = np.argmin(np.sum(np.square(self.V[:, :, None, :] - self.Y[:, None, :, :]), axis=(0, 3)), axis=1)
-            self.Z1 = self.Zeta1[self.k_star1, :]  # k_starのZの座標N*L(L=2
-            self.Z2 = self.Zeta2[self.k_star2, :]  # k_starのZの座標N*L(L=2
+                # sigma2 = self.SIGMA2_MIN + (self.SIGMA2_MAX - self.SIGMA2_MIN) * np.exp(-epoch / self.TAU2)
+                sigma2 = max(self.SIGMA2_MIN, self.SIGMA2_MAX * (1 - (epoch / self.TAU2)))
+                distance2 = distance.cdist(self.Zeta2, self.Z2, 'sqeuclidean')  # 距離行列をつくるDはN*K行列
+                H2 = np.exp(-distance2 / (2 * pow(sigma2, 2)))  # かっこに気を付ける
+                # G2 = np.sum(H2, axis=1)  # Gは行ごとの和をとったベクトル
+                # R2 = (H2.T / G2).T  # 行列の計算なので.Tで転置を行う
 
-            self.history['y'][epoch, :, :] = self.Y
-            self.history['z1'][epoch, :] = self.Z1
-            self.history['z2'][epoch, :] = self.Z2
-            self.history['sigma1'][epoch] = sigma1
-            self.history['sigma2'][epoch] = sigma2
+                G = np.einsum("ki,lj,ij->kl" ,H1, H2, self.gamma)
+
+                # ２次モデルの決定
+                self.Y = np.einsum('ki,lj,ij,ijd->kld', H1, H2, self.gamma, self.X)/G[:,:,None]
+
+                # 勝者決定
+                Dist = self.gamma[:, :, None, None, None] * np.square(self.X[:, :, None, None, :] - self.Y[None, None, :, :, :])
+                self.k_star1 = np.argmin(np.einsum("jl,ijklm->ik", H2, Dist), axis=1)
+                self.k_star2 = np.argmin(np.einsum("ik,ijklm->jl", H1, Dist), axis=1)
+                self.Z1 = self.Zeta1[self.k_star1, :]  # k_starのZの座標N*L(L=2
+                self.Z2 = self.Zeta2[self.k_star2, :]  # k_starのZの座標N*L(L=2
+
+                self.history['y'][epoch, :, :] = self.Y
+                self.history['z1'][epoch, :] = self.Z1
+                self.history['z2'][epoch, :] = self.Z2
+                self.history['sigma1'][epoch] = sigma1
+                self.history['sigma2'][epoch] = sigma2
+
+
+        else:
+            for epoch in tqdm(np.arange(nb_epoch)):
+                # 学習量の決定
+                # sigma1 = self.SIGMA1_MIN + (self.SIGMA1_MAX - self.SIGMA1_MIN) * np.exp(-epoch / self.TAU1)
+                sigma1 = max(self.SIGMA1_MIN, self.SIGMA1_MAX * (1 - (epoch / self.TAU1)))
+                distance1 = distance.cdist(self.Zeta1, self.Z1, 'sqeuclidean')  # 距離行列をつくるDはN*K行列
+                H1 = np.exp(-distance1 / (2 * pow(sigma1, 2)))  # かっこに気を付ける
+                G1 = np.sum(H1, axis=1)  # Gは行ごとの和をとったベクトル
+                R1 = (H1.T / G1).T  # 行列の計算なので.Tで転置を行う
+
+                # sigma2 = self.SIGMA2_MIN + (self.SIGMA2_MAX - self.SIGMA2_MIN) * np.exp(-epoch / self.TAU2)
+                sigma2 = max(self.SIGMA2_MIN, self.SIGMA2_MAX * (1 - (epoch / self.TAU2)))
+                distance2 = distance.cdist(self.Zeta2, self.Z2, 'sqeuclidean')  # 距離行列をつくるDはN*K行列
+                H2 = np.exp(-distance2 / (2 * pow(sigma2, 2)))  # かっこに気を付ける
+                G2 = np.sum(H2, axis=1)  # Gは行ごとの和をとったベクトル
+                R2 = (H2.T / G2).T  # 行列の計算なので.Tで転置を行う
+                # １次モデル，２次モデルの決定
+                self.U = np.einsum('lj,ijd->ild', R2, self.X)
+                self.V = np.einsum('ki,ijd->kjd', R1, self.X)
+                self.Y = np.einsum('ki,lj,ijd->kld', R1, R2, self.X)
+                # 勝者決定
+                self.k_star1 = np.argmin(np.sum(np.square(self.U[:, None, :, :] - self.Y[None, :, :, :]), axis=(2, 3)), axis=1)
+                self.k_star2 = np.argmin(np.sum(np.square(self.V[:, :, None, :] - self.Y[:, None, :, :]), axis=(0, 3)), axis=1)
+                self.Z1 = self.Zeta1[self.k_star1, :]  # k_starのZの座標N*L(L=2
+                self.Z2 = self.Zeta2[self.k_star2, :]  # k_starのZの座標N*L(L=2
+
+                self.history['y'][epoch, :, :] = self.Y
+                self.history['z1'][epoch, :] = self.Z1
+                self.history['z2'][epoch, :] = self.Z2
+                self.history['sigma1'][epoch] = sigma1
+                self.history['sigma2'][epoch] = sigma2
