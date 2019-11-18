e83648d14b702ae096ec29f9afbd7706d512f01b
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-11-18 14:50:04 +0900

    テストを書いて実行してみたがエラー

--- tests/plus_TSOM/allclose_plusTSOM.py
+++ tests/plus_TSOM/allclose_plusTSOM.py
@@ -74,27 +74,68 @@ class TestTSOMPlusSOM(unittest.TestCase):
         np.testing.assert_allclose(htsom_ishida.som.history['y'], htsom_watanabe.som.history['y'])
         np.testing.assert_allclose(htsom_ishida.som.history['z'], htsom_watanabe.som.history['z'])
 
-    def _transform_list_to_bag(self,list_of_indexes):
+    def _transform_list_to_bag(self,list_of_indexes,num_members):
+        bag_of_members = np.empty((0,num_members))
         for indexes in list_of_indexes:
-
+            one_hot_vectors = np.eye(num_members)[indexes]
+            one_bag = one_hot_vectors.sum(axis=0)[None,:]
+            bag_of_members=np.append(bag_of_members,one_bag,axis=0)
+        return bag_of_members
     def test_matching_index_member_as_list_or_bag(self):
         seed = 100
         np.random.seed(seed)
-        n_samples = 1000
+        n_members = 100
         n_groups = 10  # group数
         n_features = 3  # 各メンバーの特徴数
         n_samples_per_group = 30  # 各グループにメンバーに何人いるのか
-        member_features,index_members_of_group = self.create_artficial_data(n_samples,
+        member_features,index_members_of_group = self.create_artficial_data(n_members,
                                                                             n_features,
                                                                             n_groups,
                                                                             n_samples_per_group)
+        bag_of_members = self._transform_list_to_bag(index_members_of_group, n_members)
 
-
-        Z1 = np.random.rand(n_samples, 2) * 2.0 - 1.0
+        Z1 = np.random.rand(n_members, 2) * 2.0 - 1.0
         Z2 = np.random.rand(n_features, 2) * 2.0 - 1.0
         init_TSOM = [Z1, Z2]
         init_SOM = np.random.rand(n_groups, 2) * 2.0 - 1.0
+        params_tsom = {'latent_dim': [2, 2],
+                       'resolution': [10, 10],
+                       'SIGMA_MAX': [1.0, 1.0],
+                       'SIGMA_MIN': [0.1, 0.1],
+                       'TAU': [50, 50],
+                       'init': init_TSOM}
+        params_som = {'latent_dim': 2,
+                      'resolution': 10,
+                      'sigma_max': 2.0,
+                      'sigma_min': 0.5,
+                      'tau': 50,
+                      'init': init_SOM}
+        tsom_epoch_num = 50
+        som_epoch_num = 50
+        kernel_width = 0.3
 
+        tsom_plus_som_input_list = TSOMPlusSOM(member_features=member_features,
+                                               index_members_of_group=index_members_of_group,
+                                               params_tsom=params_tsom,
+                                               params_som=params_som)
+        tsom_plus_som_input_bag = TSOMPlusSOM(member_features=member_features,
+                                               index_members_of_group=bag_of_members,
+                                               params_tsom=params_tsom,
+                                               params_som=params_som)
+
+        tsom_plus_som_input_list.fit(tsom_epoch_num=tsom_epoch_num,
+                         kernel_width=kernel_width,
+                         som_epoch_num=som_epoch_num)
+        tsom_plus_som_input_bag.fit(tsom_epoch_num=tsom_epoch_num,
+                           kernel_width=kernel_width,
+                           som_epoch_num=som_epoch_num)
+
+        np.testing.assert_allclose(tsom_plus_som_input_list.tsom.history['y'], tsom_plus_som_input_bag.tsom.history['y'])
+        np.testing.assert_allclose(tsom_plus_som_input_list.tsom.history['z1'], tsom_plus_som_input_bag.tsom.history['z1'])
+        np.testing.assert_allclose(tsom_plus_som_input_list.tsom.history['z2'], tsom_plus_som_input_bag.tsom.history['z2'])
+        np.testing.assert_allclose(tsom_plus_som_input_list.params_som['X'], tsom_plus_som_input_bag.params_som['X'])
+        np.testing.assert_allclose(tsom_plus_som_input_list.som.history['y'], tsom_plus_som_input_bag.som.history['y'])
+        np.testing.assert_allclose(tsom_plus_som_input_list.som.history['z'], tsom_plus_som_input_bag.som.history['z'])
 
 if __name__ == "__main__":
     unittest.main()
