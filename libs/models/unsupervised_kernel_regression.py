e3dcca5551809187a5b47df7cde3b1279ab599a6
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-11-19 16:05:04 +0900

    余分なコメントアウト文の削除、変数名のrefactor

--- libs/models/unsupervised_kernel_regression.py
+++ libs/models/unsupervised_kernel_regression.py
@@ -4,48 +4,43 @@ from tqdm import tqdm
 
 
 class UnsupervisedKernelRegression(object):
-    def __init__(self, X, latent_dim,sigma=0.2,
-                 isGEM=False,
-                 isCompact=True,alpha=0.0,
-                 init='random',isLOOCV=False):
+    def __init__(self, X, n_components, bandwidth_gaussian_kernel=0.2,
+                 is_compact=True, lambda_=0.0,
+                 init='random', is_loocv=False):
         self.X = X.copy()
-        self.N = X.shape[0]
-        self.D = X.shape[1]
-        self.L = latent_dim
-        self.sigma = sigma
-        self.gamma = 1.0 / (sigma*sigma)
-        self.isCompact = isCompact
-        self.isGEM = isGEM
-        self.isLOOCV = isLOOCV
+        self.n_samples = X.shape[0]
+        self.n_dimensions = X.shape[1]
+        self.n_components = n_components
+        self.bandwidth_gaussian_kernel = bandwidth_gaussian_kernel
+        self.precision = 1.0 / (bandwidth_gaussian_kernel * bandwidth_gaussian_kernel)
+        self.is_compact = is_compact
+        self.is_loocv = is_loocv
 
         self.Z = None
         if isinstance(init, str) and init in 'random':
-            self.Z = np.random.normal(0, 0.1, (self.N, self.L))
-        elif isinstance(init, np.ndarray) and init.shape == (self.N, self.L):
+            self.Z = np.random.normal(0, 1.0, (self.n_samples, self.n_components)) * bandwidth_gaussian_kernel * 0.5
+        elif isinstance(init, np.ndarray) and init.shape == (self.n_samples, self.n_components):
             self.Z = init.copy()
         else:
             raise ValueError("invalid init: {}".format(init))
 
-        self.Alpha = alpha
+        self.lambda_ = lambda_
 
         self.history = {}
 
-        self.donefit = False
+        self._done_fit = False
 
 
 
     def fit(self, nb_epoch=100, verbose=True, eta=0.5, expand_epoch=None):
 
         K = self.X @ self.X.T
-        X2 = np.diag(K)[:, None]
-        # Xn = np.sum(np.square(self.X[:, None, :] - self.X[None, :, :]), axis=2)
-        # DistX = Xn.reshape(self.N**2, 1)
 
         self.nb_epoch = nb_epoch
 
-        self.history['z'] = np.zeros((nb_epoch, self.N, self.L))
-        self.history['y'] = np.zeros((nb_epoch, self.N, self.D))
-        self.history['zvar'] = np.zeros((nb_epoch, self.L))
+        self.history['z'] = np.zeros((nb_epoch, self.n_samples, self.n_components))
+        self.history['y'] = np.zeros((nb_epoch, self.n_samples, self.n_dimensions))
+        self.history['zvar'] = np.zeros((nb_epoch, self.n_components))
         self.history['obj_func'] = np.zeros(nb_epoch)
 
 
@@ -58,36 +53,28 @@ class UnsupervisedKernelRegression(object):
         for epoch in bar:
             Delta = self.Z[:, None, :] - self.Z[None, :, :]
             DistZ = np.sum(np.square(Delta), axis=2)
-            H = np.exp(-0.5 * self.gamma * DistZ)
-            if self.isLOOCV:
+            H = np.exp(-0.5 * self.precision * DistZ)
+            if self.is_loocv:
                 H -= np.identity(H.shape[0])
 
             # Hprime = H
             G = np.sum(H, axis=1)[:, None]
             GInv = 1 / G
             R = H * GInv
-            # Rprime = Hprime * GInv
 
             Y = R @ self.X
-            # Y2 = np.sum(np.square(Y), axis=1)[:, None]
-            # beta0 = np.sum(G) / np.sum(G * (X2 - Y2))
             DeltaYX = Y[:,None,:] - self.X[None, :, :]
             Error = Y - self.X
-            obj_func = -0.5 * np.sum(np.square(Error)) - 0.5*self.Alpha*np.sum(np.square(self.Z))
+            obj_func = -0.5 * np.sum(np.square(Error)) - self.lambda_ * np.sum(np.square(self.Z))
 
+            A = self.precision * R * np.einsum('nd,nid->ni', Y - self.X, DeltaYX)
+            dFdZ = -np.sum((A + A.T)[:, :, None] * Delta, axis=1)
 
-            A = self.gamma * R * np.einsum('nd,nid->ni', Y - self.X, DeltaYX)
-            #dFdZ = -beta0 * np.sum((A + A.T)[:, :, None] * Delta, axis=1)
-            if self.isGEM:
-                dFdZ = -np.sum(A[:, :, None] * Delta, axis=1)
-            else:
-                dFdZ = -np.sum((A + A.T)[:, :, None] * Delta, axis=1)
-
-            dFdZ -= self.Alpha * self.Z
+            dFdZ -= self.lambda_ * self.Z
 
             # self.Z += (eta / self.D) * dFdZ
             self.Z += eta * dFdZ
-            if self.isCompact:
+            if self.is_compact:
                 self.Z = np.clip(self.Z,-1.0,1.0)
             else:
                 self.Z -= self.Z.mean(axis=0)
@@ -100,7 +87,7 @@ class UnsupervisedKernelRegression(object):
 
 
 
-        self.donefit = True
+        self._done_fit = True
         return self.history
 
     def calcF(self, resolution, size='auto'):
@@ -109,25 +96,25 @@ class UnsupervisedKernelRegression(object):
         :param size:
         :return:
         """
-        if not self.donefit:
+        if not self._done_fit:
             raise ValueError("fit is not done")
 
         self.resolution = resolution
-        Zeta = create_zeta(-1, 1, self.L, resolution)
+        Zeta = create_zeta(-1, 1, self.n_components, resolution)
         M = Zeta.shape[0]
 
-        self.history['f'] = np.zeros((self.nb_epoch, M, self.D))
+        self.history['f'] = np.zeros((self.nb_epoch, M, self.n_dimensions))
 
         for epoch in range(self.nb_epoch):
             Z = self.history['z'][epoch]
             if size == 'auto':
-                Zeta = create_zeta(Z.min(), Z.max(), self.L, resolution)
+                Zeta = create_zeta(Z.min(), Z.max(), self.n_components, resolution)
             else:
-                Zeta = create_zeta(size.min(), size.max(), self.L, resolution)
+                Zeta = create_zeta(size.min(), size.max(), self.n_components, resolution)
 
             Dist = dist.cdist(Zeta, Z, 'sqeuclidean')
 
-            H = np.exp(-0.5 *self.gamma* Dist)
+            H = np.exp(-0.5 * self.precision * Dist)
             G = np.sum(H, axis=1)[:, None]
             GInv = np.reciprocal(G)
             R = H * GInv
@@ -140,7 +127,7 @@ class UnsupervisedKernelRegression(object):
         # calculate latent variables of new data using gradient descent
         # objective function is square error E = ||f(z)-x||^2
 
-        if not self.donefit:
+        if not self._done_fit:
             raise ValueError("fit is not done")
 
         Nnew = Xnew.shape[0]
@@ -159,7 +146,7 @@ class UnsupervisedKernelRegression(object):
             # calculate gradient
             Delta = self.Z[None,:,:] - Znew[:,None,:]                   # shape = (Nnew,N,L)
             Dist_Znew_Z = dist.cdist(Znew,self.Z,"sqeuclidean")         # shape = (Nnew,N)
-            H = np.exp(-0.5 *self.gamma* Dist_Znew_Z)                              # shape = (Nnew,N)
+            H = np.exp(-0.5 * self.precision * Dist_Znew_Z)                              # shape = (Nnew,N)
             G = np.sum(H,axis=1)[:,None]                                # shape = (Nnew,1)
             Ginv = np.reciprocal(G)                                     # shape = (Nnew,1)
             R = H * Ginv                                                # shape = (Nnew,N)
@@ -167,14 +154,14 @@ class UnsupervisedKernelRegression(object):
 
             Delta_bar = np.einsum("kn,knl->kl",R,Delta)                 # (Nnew,N)times(Nnew,N,L)=(Nnew,L)
             # Delta_bar = np.sum(R[:,:,None] * Delta, axis=1)           # same calculate
-            dRdZ = self.gamma * R[:,:,None] * (Delta - Delta_bar[:,None,:])          # shape = (Nnew,N,L)
+            dRdZ = self.precision * R[:, :, None] * (Delta - Delta_bar[:, None, :])          # shape = (Nnew,N,L)
 
             dFdZ = np.einsum("nd,knl->kdl",self.X,dRdZ)                 # shape = (Nnew,D,L)
             # dFdZ = np.sum(self.X[None,:,:,None]*dRdZ[:,:,None,:],axis=1)  # same calculate
             dEdZ = 2.0 * np.einsum("kd,kdl->kl",F-Xnew,dFdZ) # shape (Nnew, L)
             # update latent variables
             Znew -= eta_trans * dEdZ
-            if self.isCompact:
+            if self.is_compact:
                 Znew = np.clip(Znew,-1.0,1.0)
             if constrained:
                 Znew = np.clip(Znew, self.Z.min(axis=0), self.Z.max(axis=0))
@@ -182,13 +169,13 @@ class UnsupervisedKernelRegression(object):
         return Znew
 
     def inverse_transform(self, Znew):
-        if not self.donefit:
+        if not self._done_fit:
             raise ValueError("fit is not done")
-        if Znew.shape[1]!=self.L:
-            raise ValueError("Znew dimension must be {}".format(self.L))
+        if Znew.shape[1]!=self.n_components:
+            raise ValueError("Znew dimension must be {}".format(self.n_components))
 
         Dist_Znew_Z = dist.cdist(Znew,self.Z,"sqeuclidean")         # shape = (Nnew,N)
-        H = np.exp(-0.5 * self.gamma *Dist_Znew_Z)                              # shape = (Nnew,N)
+        H = np.exp(-0.5 * self.precision * Dist_Znew_Z)                              # shape = (Nnew,N)
         G = np.sum(H,axis=1)[:,None]                                # shape = (Nnew,1)
         Ginv = np.reciprocal(G)                                     # shape = (Nnew,1)
         R = H * Ginv                                                # shape = (Nnew,N)
