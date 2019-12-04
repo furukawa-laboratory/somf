a70776fb9e92ec89508a9f33a7758b5e24922d15
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2019-12-04 17:49:57 +0900

    カーネル密度推定の計算をfit以外でも利用できるように一般化、テストを実行しpassを確認

--- libs/models/tsom_plus_som.py
+++ libs/models/tsom_plus_som.py
@@ -23,28 +23,36 @@ class TSOMPlusSOM:
         self.tsom.fit(tsom_epoch_num)
 
     def _fit_KDE(self, kernel_width):  # 学習した後の潜在空間からKDEで確率分布を作る
-        prob_data = np.zeros((self.group_num, self.tsom.K1))  # group数*ノード数
+        prob_data = self._calculate_kde(group_features=self.group_features,kernel_width=kernel_width)
+        self.params_som['X'] = prob_data
+        self.params_som['metric'] = "KLdivergence"
+
+    def _calculate_kde(self, group_features, kernel_width):
         # グループごとにKDEを適用
-        if isinstance(self.group_features, np.ndarray) and self.group_features.ndim == 2:
+        if isinstance(group_features, np.ndarray) and group_features.ndim == 2:
             # group_featuresがbag of membersで与えられた時の処理
             distance = dist.cdist(self.tsom.Zeta1, self.tsom.Z1, 'sqeuclidean')  # K1 x num_members
             H = np.exp(-0.5 * distance / (kernel_width * kernel_width))  # KxN
-            prob_data = self.group_features @ H.T  # num_group x K1
+            prob_data = group_features @ H.T  # num_group x K1
             prob_data = prob_data / prob_data.sum(axis=1)[:, None]
         else:
             # group_featuresがlist of listsもしくはlist of arraysで与えられた時の処理
-            for i in range(self.group_num):
+            prob_data = np.zeros((self.group_num, self.tsom.K1))  # group数*ノード数
+            for i,one_group_features in enumerate(group_features):
                 Dist = dist.cdist(self.tsom.Zeta1,
-                                  self.tsom.Z1[self.group_features[i], :],
+                                  self.tsom.Z1[one_group_features, :],
                                   'sqeuclidean')  # KxNの距離行列を計算
-                H = np.exp(-Dist / (2 * kernel_width * kernel_width))  # KxNの学習量行列を計算
+                H = np.exp(-Dist / (2 * kernel_width * kernel_width))  # KxNのカーネルの値を計算
                 prob = np.sum(H, axis=1)
                 prob_sum = np.sum(prob)
                 prob = prob / prob_sum
                 prob_data[i, :] = prob
-        self.params_som['X'] = prob_data
-        self.params_som['metric'] = "KLdivergence"
+        return prob_data
+
 
     def _fit_2nd_SOM(self, som_epoch_num):  # 上位のSOMを
         self.som = SOM(**self.params_som)
         self.som.fit(som_epoch_num)
+
+    def transform(self, group_features):
+        pass
