87371c23f23b4bc76140272435834c28bd58726c
tsuno0829 <tsnry7913@gmail.com>
2020-01-31 00:33:13 +0900

    initでyの初期化を行い，fitではzから更新できるように変更した

--- libs/models/som.py
+++ libs/models/som.py
@@ -6,7 +6,7 @@ from sklearn.decomposition import PCA
 
 
 class SOM:
-    def __init__(self, X, latent_dim, resolution, sigma_max, sigma_min, tau, init='random',metric="sqeuclidean"):
+    def __init__(self, X, latent_dim, resolution, sigma_max, sigma_min, tau, init='random', metric="sqeuclidean"):
         self.X = X
         self.N = self.X.shape[0]
 
@@ -16,6 +16,8 @@ class SOM:
 
         self.D = X.shape[1]
         self.L = latent_dim
+
+        self.UpdateFromY = True
 
         self.history = {}
 
@@ -39,6 +41,9 @@ class SOM:
 
         if isinstance(init, str) and init == 'random':
             self.Z = np.random.rand(self.N, latent_dim) * 2.0 - 1.0
+        elif isinstance(init, str) and init == 'random_y':
+            self.Y = np.random.rand(self.K, self.D) * 2.0 - 1.0
+            self.UpdateFromY = False
         elif isinstance(init, str) and init == 'random_bmu':
             init_bmus = np.random.randint(0, self.Zeta.shape[0] - 1, self.N)
             self.Z = self.Zeta[init_bmus,:]
@@ -107,12 +112,14 @@ class SOM:
             bar = range(nb_epoch)
 
         for epoch in bar:
-            # 協調過程
-            self._cooperative_process(epoch)
-            # 適合過程
-            self._adaptive_process()
-            # 競合過程
-            self._competitive_process()
+            if self.UpdateFromY:
+                self._cooperative_process(epoch)   # 協調過程
+                self._adaptive_process()           # 適合過程
+                self._competitive_process()        # 競合過程
+            else:
+                self._competitive_process()        # 競合過程
+                self._cooperative_process(epoch)   # 協調過程
+                self._adaptive_process()           # 適合過程
 
             self.history['z'][epoch] = self.Z
             self.history['y'][epoch] = self.Y
