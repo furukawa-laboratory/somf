2717f90916eebc4c4242136ca5a3e8df296d95af
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2020-03-11 18:24:57 +0900

    テストコードでモデルの2通りと欠損有る無しの2通りの組み合わせ4通りを試すように変更

--- tests/tsom/allclose_missing_kusumoto_vs_missing_ishida.py
+++ tests/tsom/allclose_missing_kusumoto_vs_missing_ishida.py
@@ -18,15 +18,6 @@ class TestTSOM_missing(unittest.TestCase):
         observed_dim = 3
 
         X = np.random.normal(0, 1, (nb_samples1, nb_samples2, observed_dim))
-        #gammaの生成
-        gamma=np.random.rand(nb_samples1, nb_samples2, observed_dim)
-        for i in np.arange(nb_samples1):
-            for j in np.arange(nb_samples2):
-                for k in np.arange(observed_dim):
-                    if gamma[i,j,k]>=0.5:
-                        gamma[i,j,k]=1
-                    elif gamma[i,j,k]<0.5:
-                        gamma[i, j,k] = 0
         # set learning parameter
         nb_epoch = 250
         latent_dim = [1, 1]
@@ -34,6 +25,8 @@ class TestTSOM_missing(unittest.TestCase):
         sigma_max = [2.0, 2.2]
         sigma_min = [0.4, 0.2]
         tau = [50, 60]
+        model_list = ['direct','indirect']
+        gamma_list = [None, np.random.choice([0,1],size=(nb_samples1,nb_samples2))]
 
         ## prepare init
         Z1init = np.random.rand(nb_samples1, latent_dim[0])
@@ -41,21 +34,23 @@ class TestTSOM_missing(unittest.TestCase):
         init = [Z1init, Z2init]
 
         # generate tsom instance
-        tsom_kusumoto = TSOM2(X, latent_dim=latent_dim, resolution=resolution,
-                           SIGMA_MAX=sigma_max, SIGMA_MIN=sigma_min, TAU=tau,
-                           init=init,model = 'direct',gamma=gamma)
-        tsom_ishida = TSOM2_ishida(X, latent_dim=latent_dim, resolution=resolution,
+        for model in model_list:
+            for gamma in gamma_list:
+                tsom_kusumoto = TSOM2(X, latent_dim=latent_dim, resolution=resolution,
                                    SIGMA_MAX=sigma_max, SIGMA_MIN=sigma_min, TAU=tau,
-                                   init=init,model = 'direct',gamma=gamma)
+                                   init=init,model = model,gamma=gamma)
+                tsom_ishida = TSOM2_ishida(X, latent_dim=latent_dim, resolution=resolution,
+                                           SIGMA_MAX=sigma_max, SIGMA_MIN=sigma_min, TAU=tau,
+                                           init=init,model = model,gamma=gamma)
 
-        # learn
-        tsom_kusumoto.fit(nb_epoch=nb_epoch)
-        tsom_ishida.fit(nb_epoch=nb_epoch)
+                # learn
+                tsom_kusumoto.fit(nb_epoch=nb_epoch)
+                tsom_ishida.fit(nb_epoch=nb_epoch)
 
-        # test
-        np.testing.assert_allclose(tsom_kusumoto.history['y'], tsom_ishida.history['y'],rtol=1e-09)
-        np.testing.assert_allclose(tsom_kusumoto.history['z1'], tsom_ishida.history['z1'])
-        np.testing.assert_allclose(tsom_kusumoto.history['z2'], tsom_ishida.history['z2'])
+                # test
+                np.testing.assert_allclose(tsom_kusumoto.history['y'], tsom_ishida.history['y'],rtol=1e-09)
+                np.testing.assert_allclose(tsom_kusumoto.history['z1'], tsom_ishida.history['z1'])
+                np.testing.assert_allclose(tsom_kusumoto.history['z2'], tsom_ishida.history['z2'])
 
 if __name__ == "__main__":
     unittest.main()
