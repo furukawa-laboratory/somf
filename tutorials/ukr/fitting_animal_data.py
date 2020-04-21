de6a05c420eeb9643d88a207723cf7cbe36f9e58
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2020-04-21 11:22:44 +0900

    refactorに伴うtutorial code内の変数名の変更 and 初期値をモデル任せではなくcode内で指定

--- tutorials/ukr/fitting_animal_data.py
+++ tutorials/ukr/fitting_animal_data.py
@@ -2,29 +2,31 @@ import numpy as np
 
 from libs.models.unsupervised_kernel_regression import UnsupervisedKernelRegression as UKR
 from libs.datasets.artificial import animal
+from sklearn.utils import check_random_state
 
 if __name__ == '__main__':
     n_components = 2
-    bandwidth = 0.5
+    bandwidth = 0.4
     lambda_ = 0.0
     is_compact = True
     is_save_history = True
 
-    nb_epoch = 100
-    eta = 5.0
+    nb_epoch = 1000
+    eta = 0.02
 
-    resolution = 100
+    n_grid_points = 100
 
     X, labels_animal, labels_feature = animal.load_data(retlabel_animal=True,
                                                         retlabel_feature=True)
 
-    seed = 13
-    np.random.seed(seed)
+    seed = 12
+    random_state = check_random_state(seed)
+    init = random_state.normal(0.0, bandwidth * 0.1, size=(X.shape[0], n_components))
 
     ukr = UKR(X, n_components=n_components, bandwidth_gaussian_kernel=bandwidth,
-              is_compact=is_compact, is_save_history=is_save_history, lambda_=lambda_)
+              is_compact=is_compact, is_save_history=is_save_history, lambda_=lambda_, init=init)
     ukr.fit(nb_epoch=nb_epoch, eta=eta)
 
-    ukr.visualize(n_representative_points=resolution,
-                  label_data = labels_animal,
+    ukr.visualize(n_grid_points=n_grid_points,
+                  label_data=labels_animal,
                   label_feature=labels_feature)
