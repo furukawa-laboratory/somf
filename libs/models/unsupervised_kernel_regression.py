825ae819a035581b64ac1219d4c68a46396655a0
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2020-04-22 09:15:02 +0900

    Edit error message

--- libs/models/unsupervised_kernel_regression.py
+++ libs/models/unsupervised_kernel_regression.py
@@ -207,7 +207,7 @@ class UnsupervisedKernelRegression(object):
             # 代表点の数を潜在空間の次元ごとに格納
             self.n_grid_points = np.ones(self.n_components, dtype='int8') * n_grid_points
         else:
-            raise ValueError('Only support n_reprsentative_point is int')
+            raise ValueError('Only support n_grid_points is int')
         if self.is_compact:
             self.grid_points = create_zeta(-1.0, 1.0, self.n_components, n_grid_points)
         else:
