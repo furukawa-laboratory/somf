524ede2e9329caa74803d2fbb5aacf1a32086228
tsuno0829 <tsnry7913@gmail.com>
2020-05-18 21:11:03 +0900

    途中経過を保存．画像と多様体の座標が一致してない

--- tutorials/som2/face_angle_PCA.py
+++ tutorials/som2/face_angle_PCA.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+from matplotlib import offsetbox
 from PIL import Image
 from sklearn.decomposition import PCA
 from libs.models.som2 import SOM2
@@ -12,7 +14,7 @@ from libs.models.som2 import SOM2
 if __name__ == "__main__":
     seed = 1
     np.random.seed(seed)
-    nb_epoch = 20
+    nb_epoch = 1
     max_angle = 80
     pixel_size = 64
     n_class = 90
@@ -21,7 +23,7 @@ if __name__ == "__main__":
     parent_latent_dim = 2
     child_latent_dim = 1
     parent_resolution = 10
-    child_resolution = 100
+    child_resolution = 50
     pCluster_num = parent_resolution ** parent_latent_dim
     cCluster_num = child_resolution ** child_latent_dim
     parent_sigma_max = 2.0
@@ -91,29 +93,60 @@ if __name__ == "__main__":
     cZ = model.history["cZ"]
     pZ = model.history["pZ"]
     pZeta = model.history["pZeta"]
-    bmu = model.history["bmu"]
+    bmu = model.history["bmu"].astype(np.int64)
+    bmm = model.history["bmm"].astype(np.int64)
 
     fig = plt.figure(figsize=(6, 6))
-    ax = fig.add_subplot(111, projection='3d')
+    ax1 = fig.add_subplot(111, projection='3d')
+    ax2 = fig.add_subplot(111, frame_on=False)
+    ax2.axis("off")
+    ax2.axis([0,1,0,1])
+
+    def proj(X, ax1, ax2):
+        x, y, z = X
+        x2, y2, _ = proj3d.proj_transform(x, y, z, ax1.get_proj())
+        return ax2.transData.inverted().transform(ax1.transData.transform((x2, y2)))
+
+    def image(ax, arr, xy):
+        im = offsetbox.OffsetImage(arr, zoom=0.05)
+        im.image.axes = ax
+        ab = offsetbox.AnnotationBbox(im, xy, xybox=(30, 30),
+                                    xycoords="data", boxcoords="offset points",
+                                    pad=0, arrowprops=dict(arrowstyle="->"))
+        ax.add_artist(ab)
 
     def update(epoch):
-        ax.cla()
+        ax1.cla()
+        ax2.cla()
         display_manifold_idx = np.linspace(0, n_class-1, 3, dtype="int64")
         for i in display_manifold_idx:
-            transformed_cY = cY[epoch, i] @ U
-            ax.scatter(transformed_cY[:, 0], transformed_cY[:, 1], transformed_cY[:, 2])
-            # for j in bmu[epoch]:
-            #     j = int(j)
-            #     ax.text(transformed_cY[j, 0], transformed_cY[j, 1], transformed_cY[j, 2], j)
+            trans_cY = cY[epoch, i] @ U
+            # trans_cY = cY[epoch, bmu[epoch, i]] @ U
+            ax1.scatter(trans_cY[:, 0], trans_cY[:, 1], trans_cY[:, 2])
+            # for j in np.linspace(0, n_sample-1, 3, dtype="int64"):
+            #     # if i in np.linspace(0, cCluster_num-1, 5, dtype="int64"):
+            #     img = Datasets[i, j].reshape(pixel_size, pixel_size)
+            #     img = Image.fromarray(img).resize((512, 512))
+            #     img = np.array(img)
+            #     x, y = proj([trans_cY[bmu[epoch, i, j], 0], trans_cY[bmu[epoch, i, j], 1], trans_cY[bmu[epoch, i, j], 2]], ax1, ax2)
+            #     image(ax2, img, [x, y])
+            for j in np.linspace(0, n_sample-1, 3, dtype="int64"):
+                # if i in np.linspace(0, cCluster_num-1, 5, dtype="int64"):
+                img = Datasets[i, j].reshape(pixel_size, pixel_size)
+                img = Image.fromarray(img).resize((512, 512))
+                img = np.array(img)
+                x, y = proj([trans_cY[bmu[epoch, i, j], 0], trans_cY[bmu[epoch, i, j], 1], trans_cY[bmu[epoch, i, j], 2]], ax1, ax2)
+                image(ax2, img, [x, y])
+
         # fiberの表示
         for i in np.linspace(0, cCluster_num-1, 5, dtype="int64"):
             transformed_cY = cY[epoch, :, i] @ U
             transformed_cY = transformed_cY[display_manifold_idx]
-            ax.plot(transformed_cY[:, 0], transformed_cY[:, 1], transformed_cY[:, 2], color='gray')
+            ax1.plot(transformed_cY[:, 0], transformed_cY[:, 1], transformed_cY[:, 2], color='gray')
 
-        ax.set_xlabel("component1")
-        ax.set_ylabel("component2")
-        ax.set_zlabel("component3")
+        ax1.set_xlabel("component1")
+        ax1.set_ylabel("component2")
+        ax1.set_zlabel("component3")
 
         fig.suptitle(
             "epoch {}/{} observation space".format((epoch + 1), nb_epoch),
@@ -123,5 +156,5 @@ if __name__ == "__main__":
     ani = anim.FuncAnimation(
         fig, update, interval=interval, frames=nb_epoch, repeat=False
     )
-    ani.save("SOM2_PCA.gif", writer='pillow')
-    # plt.show()
+    # ani.save("SOM2_PCA.gif", writer='pillow')
+    plt.show()
