1d13a1ac68fffa1fe37c8211a3a3ed4f2374024c
tsuno0829 <tsnry7913@gmail.com>
2020-06-29 17:27:48 +0900

    curve2.pyもnew SOM2に対応させた

--- tutorials/som2/curve2.py
+++ tutorials/som2/curve2.py
@@ -8,52 +8,65 @@ import matplotlib.animation as anim
 from libs.models.som2 import SOM2
 
 if __name__ == "__main__":
-    seed = 2
+    seed = 1
     np.random.seed(seed)
     nb_epoch = 20
     n_class = 6
-    n_sample = 400
+    n_sample_list = np.random.randint(low=100, high=200, size=n_class)
     Dim = 2
     parent_latent_dim = 2
     child_latent_dim = 1
     parent_resolution = 8
     child_resolution = 100
-    pCluster_num = parent_resolution ** parent_latent_dim
-    cCluster_num = child_resolution ** child_latent_dim
+    parent_node_num = parent_resolution ** parent_latent_dim
+    child_node_num = child_resolution ** child_latent_dim
     parent_sigma_max = 2.0
-    parent_sigma_min = 0.2
+    parent_sigma_min = 0.4
     child_sigma_max = 2.0
-    child_sigma_min = 0.1
+    child_sigma_min = 0.15
     parent_tau = nb_epoch
     child_tau = nb_epoch
-    interval = 500
-
-    # ZetaとZの初期値を生成
-    if parent_latent_dim == 2:
-        pZ = np.random.normal(size=(n_class, parent_latent_dim), loc=0, scale=0.01)
-    else:
-        pZ = np.random.normal(size=(n_class, 1), loc=0.0, scale=0.01)
-
-    if child_latent_dim == 2:
-        cZ = np.random.normal(size=(n_sample, child_latent_dim), loc=0, scale=0.01)
-    else:
-        cZ = np.random.normal(size=(n_sample, 1), loc=0.0, scale=0.01)
+    interval = 50
 
     # データ生成
-    Datasets = np.zeros((n_class, n_sample, Dim))
-    x = np.random.rand(n_class, n_sample) * 2 - 1
-    x = np.sort(x)
-    y = [x[0] ** 3 - 0.5 * x[0], -x[1] ** 3 + 0.5 * x[1], x[2] ** 3 - 0.5 * x[2], -x[3] ** 3 + 0.5 * x[3],
-         x[4] ** 3 - 0.5 * x[4], -x[5] ** 3 + 0.5 * x[5]]
-    t = 0
-    for i in [+1, +1, 0, 0, -1, -1]:
-        Datasets[t, :, 0] = x[t] + i
-        Datasets[t, :, 1] = y[t] + i
-        t += 1
+    datasets = []
+    func = [
+        lambda x, n: x ** 3 - 0.5 * x + n,
+        lambda x, n: -(x ** 3) + 0.5 * x + n,
+    ]
 
-    model = SOM2(Datasets, parent_latent_dim, child_latent_dim, parent_resolution, child_resolution,
-                 parent_sigma_max, child_sigma_max, parent_sigma_min, child_sigma_min,
-                 parent_tau, child_tau, pZ, cZ, is_save_history=True)
+
+    shift = [1, 1, 0, 0, -1, -1]
+    for n, n_sample in enumerate(n_sample_list):
+        x = np.random.rand(n_sample) * 3 - 1.5
+        x = np.sort(x)
+        y = list(map(func[n % 2], x, [shift[n]] * len(x)))
+        x = list(map(lambda x: x+shift[n], x))
+        datasets.append(np.dstack([x, y]).tolist()[0])
+
+    # Zの初期値を生成
+    init_bmus = []
+    for n_sample in n_sample_list:
+        init_bmus.append(np.random.randint(low=0, high=child_node_num, size=n_sample, dtype="int"))
+    init_bmm = np.random.randint(low=0, high=parent_node_num, size=n_class, dtype="int")
+
+    params_1st_som = {
+        "latent_dim": child_latent_dim,
+        "resolution": child_resolution,
+        "sigma_max": child_sigma_max,
+        "sigma_min": child_sigma_min,
+        "tau": child_tau,
+    }
+
+    params_2nd_som = {
+        "latent_dim": parent_latent_dim,
+        "resolution": parent_resolution,
+        "sigma_max": parent_sigma_max,
+        "sigma_min": parent_sigma_min,
+        "tau": parent_tau,
+    }
+
+    model = SOM2(datasets, params_1st_som, params_2nd_som, init_bmus, init_bmm, is_save_history=True)
     model.fit(nb_epoch)
 
     cY = model.history["cY"]
@@ -71,7 +84,7 @@ if __name__ == "__main__":
                 axes[i, j].cla()
 
         unique_bmm = np.unique(bmm[epoch])
-        list_bmm = [0]*pCluster_num
+        list_bmm = [0]*parent_node_num
         for i in unique_bmm:
             list_bmm[int(i)] = 1
 
@@ -88,8 +101,8 @@ if __name__ == "__main__":
                 t += 1
                 axes[i, j].set_xticks([])
                 axes[i, j].set_yticks([])
-                axes[i, j].set_xlim(-2, 2)
-                axes[i, j].set_ylim(-2, 2)
+                axes[i, j].set_xlim(-3, 3)
+                axes[i, j].set_ylim(-3, 3)
 
         fig.suptitle("epoch {}/{} latent space(parent)".format((epoch + 1), nb_epoch), fontsize=10)
 
