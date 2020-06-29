a0b2af38b288837215d1166b9ca8e91c89935846
tsuno0829 <tsnry7913@gmail.com>
2020-06-29 11:38:30 +0900

    データを可変長で生成するように変更した．また，Zの初期値の生成は一旦削除した

--- tutorials/som2/triangle3d.py
+++ tutorials/som2/triangle3d.py
@@ -14,51 +14,40 @@ if __name__ == "__main__":
     np.random.seed(seed)
     nb_epoch = 100
     n_class = 3
-    n_sample = 200
+    n_sample_list = np.random.randint(low=100, high=150, size=n_class)
     Dim = 3
     parent_latent_dim = 1
-    child_latent_dim = 2
+    child_latent_dim = 1
     parent_resolution = 5
     child_resolution = 10
     parent_node_num = parent_resolution ** parent_latent_dim
     child_node_num = child_resolution ** child_latent_dim
     parent_sigma_max = 2.0
-    parent_sigma_min = 0.4
+    parent_sigma_min = 0.5
     child_sigma_max = 2.0
     child_sigma_min = 0.2
     parent_tau = nb_epoch
     child_tau = nb_epoch
     interval = 100
 
-    # Zの初期値を生成
-    if parent_latent_dim == 2:
-        pZ = np.random.normal(size=(n_class, parent_latent_dim), loc=0, scale=0.01)
-    else:
-        pZ = np.random.normal(size=(n_class, 1), loc=0.0, scale=0.01)
-
-    if child_latent_dim == 2:
-        cZ = np.random.normal(size=(n_sample, child_latent_dim), loc=0, scale=0.01)
-    else:
-        cZ = np.random.normal(size=(n_sample, 1), loc=0.0, scale=0.01)
-
     # データ生成
-    datasets = np.zeros((n_class, n_sample, Dim))
+    datasets = []
     theta = np.linspace(-np.pi / 12, np.pi / 12, n_class)
     for n in range(n_class):
+        dataset = []
         min_X, max_X = 0, 4
         min_Y, max_Y = -1, 1
-        X = np.random.uniform(min_X, max_X, n_sample)
-        Y = np.zeros(n_sample)
-        for s in range(n_sample):
+        X = np.random.uniform(min_X, max_X, n_sample_list[n])
+        Y = np.zeros(n_sample_list[n])
+        for s in range(n_sample_list[n]):
             deltaY = (max_Y * X[s]) / max_X
             Y[s] = np.random.uniform(-deltaY, deltaY)
         rotate_X = X * np.cos(theta[n]) + Y * np.sin(theta[n])
         rotate_Y = X * np.sin(theta[n]) - Y * np.cos(theta[n])
         rotate_X -= np.mean(rotate_X)
         rotate_Y -= np.mean(rotate_Y)
-        datasets[n][:, 0] = rotate_X
-        datasets[n][:, 1] = rotate_Y
-        datasets[n][:, 2] = n - n_class / 2
+        stack = np.dstack([rotate_X, rotate_Y, [n - n_class / 2] * n_sample_list[n]])
+        datasets.append(stack[0])
 
     params_1st_som = {
         "latent_dim": child_latent_dim,
@@ -66,7 +55,7 @@ if __name__ == "__main__":
         "sigma_max": child_sigma_max,
         "sigma_min": child_sigma_min,
         "tau": child_tau,
-        "init": cZ,
+        "init": "random",
     }
 
     params_2nd_som = {
@@ -75,7 +64,7 @@ if __name__ == "__main__":
         "sigma_max": parent_sigma_max,
         "sigma_min": parent_sigma_min,
         "tau": parent_tau,
-        "init": pZ,
+        "init": "random",
     }
 
     model = SOM2(datasets, params_1st_som, params_2nd_som, is_save_history=True)
@@ -89,7 +78,7 @@ if __name__ == "__main__":
     bmu = model.history["bmu"]
 
     # 描画
-    fig = plt.figure(figsize=(10, 5))
+    fig = plt.figure(figsize=(8, 4))
     gs_master = GridSpec(nrows=1, ncols=2)
     gs_1 = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[:, 0:1])
     gs_2 = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[:, 1:2])
@@ -101,7 +90,7 @@ if __name__ == "__main__":
         ax2.cla()
 
         for n in range(n_class):
-            ax1.scatter(datasets[n, :, 0], datasets[n, :, 1], datasets[n, :, 2], c=datasets[n, :, 0],
+            ax1.scatter(datasets[n][:, 0], datasets[n][:, 1], datasets[n][:, 2], c=datasets[n][:, 0],
                         cmap="viridis", marker="+", label='observation data')
 
             if parent_latent_dim == 2:
@@ -113,8 +102,11 @@ if __name__ == "__main__":
 
 
         for k in range(parent_node_num):
-            py = pY[epoch, k].reshape(child_resolution, child_resolution, Dim)
-            ax1.plot_wireframe(py[:, :, 0], py[:, :, 1], py[:, :, 2], color='r')
+            if child_latent_dim == 2:
+                py = pY[epoch, k].reshape(child_resolution, child_resolution, Dim)
+                ax1.plot_wireframe(py[:, :, 0], py[:, :, 1], py[:, :, 2], color='r')
+            else:
+                ax1.plot(pY[epoch, k, :, 0], pY[epoch, k, :, 1], pY[epoch, k, :, 2], color='r')
 
         # fiberの表示
         for i in [0, child_resolution-1, child_node_num-child_resolution, child_node_num-1]:
